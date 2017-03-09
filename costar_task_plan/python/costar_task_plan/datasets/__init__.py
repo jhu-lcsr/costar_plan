7d9912d24744e45955dae6028f4740291cf09318
Chris Paxton <cpaxton3@jhu.edu>
2017-03-08 17:10:31 -0800

    move "ros" stuff

--- costar_task_plan/python/costar_task_plan/datasets/__init__.py
+++ costar_task_plan/python/costar_task_plan/datasets/__init__.py
@@ -2,6 +2,4 @@ from tools import *
 from dataset import Config
 from dataset import Dataset
 from visr import VisrDataset
-from redacted import RedactedDataset
-from redacted import RedactedConfig
 from tom import TomDataset
