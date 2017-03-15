43483dcacb8b1a8d1d4e8fd85bebb1b8305eb346
Chris Paxton <cpaxton3@jhu.edu>
2017-03-15 15:55:04 -0700

    fixes and formatting in supervised trainer

--- costar_task_plan/python/costar_task_plan/trainers/basic.py
+++ costar_task_plan/python/costar_task_plan/trainers/basic.py
@@ -2,7 +2,7 @@ from abstract import *
 from util import *
 import numpy as np
 
-import costar_task_plan.datasets.tools as dt
+import task_tree_search.datasets.tools as dt
 
 '''
 Default trainer class.
@@ -10,6 +10,8 @@ Default trainer class.
 Does a bunch of rollouts, keeps the good ones, and fits a supervised model to
 them that will predict actions.
 '''
+
+
 class SupervisedTrainer(AbstractTrainer):
 
     def __init__(self, policy, actor, R_threshold, *args, **kwargs):
@@ -21,54 +23,54 @@ class SupervisedTrainer(AbstractTrainer):
         self.train_test_split = 0.8
 
     def _step(self, data, *args, **kwargs):
-      X, Y = [], []
-      for samples in data:
-        for sample in samples:
-          # only include good examples in the training data
-          if sample.R > self.R_threshold:
-            X.append(sample.f0)
-            Y.append(sample.a0.toArray())
-      X = np.array(X)
-      Y = np.array(Y)
+        X, Y = [], []
+        for samples in data:
+            for sample in samples:
+                # only include good examples in the training data
+                if sample.R > self.R_threshold:
+                    X.append(sample.s0)
+                    Y.append(sample.a0)
+        X = np.array(X)
+        Y = np.array(Y)
 
-      # get the list of indices
-      idx = range(X.shape[0])
-      np.random.shuffle(idx)
+        # get the list of indices
+        idx = range(X.shape[0])
+        np.random.shuffle(idx)
 
-      # split into train and test data
-      num_train = int(self.train_test_split * len(idx))
-      train_idx = idx[:num_train]
-      test_idx = idx[num_train:]
+        # split into train and test data
+        num_train = int(self.train_test_split * len(idx))
+        train_idx = idx[:num_train]
+        test_idx = idx[num_train:]
 
-      X_train = X[train_idx]
-      Y_train = Y[train_idx]
-      X_test = X[test_idx]
-      Y_test = Y[test_idx]
+        X_train = X[train_idx]
+        Y_train = Y[train_idx]
+        X_test = X[test_idx]
+        Y_test = Y[test_idx]
 
-      self.actor.fit(X_train, Y_train,
-          validation_data=(X_test, Y_test),
-          *args, **kwargs)
+        self.actor.fit(X_train, Y_train,
+                       validation_data=(X_test, Y_test),
+                       *args, **kwargs)
 
     # draw a random action from our current model
     def sample(self, state):
-      # actually just ignores the state
-      world = self.env._world
-      actor = world.getLearner()
-      state = actor.state
-      action = self.policy.evaluate(world, state, actor)
-      return action.toArray(), 1
+        # actually just ignores the state
+        world = self.env._world
+        actor = world.getLearner()
+        state = actor.state
+        action = self.policy.evaluate(world, state, actor)
+        return action.toArray(), 1
 
     def save(self, filename):
-      self.actor.save_weights(filename + "_actor.h5f")
+        self.actor.save_weights(filename + "_actor.h5f")
 
-    def compile(self,*args,**kwargs):
-      self.actor.compile(*args,**kwargs)
+    def compile(self, *args, **kwargs):
+        self.actor.compile(*args, **kwargs)
 
     def getActorModel(self):
-      return self.actor
+        return self.actor
 
     def getAllModels(self):
-      return [self.actor]
+        return [self.actor]
 
 
 '''
@@ -76,61 +78,63 @@ Modified version for recurrent networks.
 
 We need to modify the step() function primarily
 '''
+
+
 class SupervisedRecurrentTrainer(SupervisedTrainer):
 
-  def __init__(self, policy, actor, R_threshold, window_length,
-      *args, **kwargs):
-    print kwargs
-    super(SupervisedRecurrentTrainer, self).__init__(
-        policy,
-        actor,
-        R_threshold,
-        *args, **kwargs)
-    if not is_recurrent(self.actor):
-      raise RuntimeError('You are using the recurrent trainer with a ' + \
-          'non-recurrent model. That is a weird choice.')
-    self.window_length = window_length
+    def __init__(self, policy, actor, R_threshold, window_length,
+                 *args, **kwargs):
+        print kwargs
+        super(SupervisedRecurrentTrainer, self).__init__(
+            policy,
+            actor,
+            R_threshold,
+            *args, **kwargs)
+        if not is_recurrent(self.actor):
+            raise RuntimeError('You are using the recurrent trainer with a ' +
+                               'non-recurrent model. That is a weird choice.')
+        self.window_length = window_length
 
-  def _step(self, data, *args, **kwargs):
-    X, Y = None, None
-    for samples in data:
-      # individual batches
-      x, y = [], []
-      for sample in samples:
-        # only include good examples in the training data
-        if sample.R > self.R_threshold:
-          x.append(sample.s0)
-          y.append(sample.a0)
+    def _step(self, data, *args, **kwargs):
+        X, Y = None, None
+        for samples in data:
+            # individual batches
+            x, y = [], []
+            for sample in samples:
+            # only include good examples in the training data
+                if sample.R > self.R_threshold:
+                    x.append(sample.s0)
+                    y.append(sample.a0)
 
-      if len(x) == 0:
-        continue
+            if len(x) == 0:
+                continue
 
-      x = dt.to_recurrent_samples(x, self.window_length)
-      y = np.array(y[self.window_length-1:])
-      if X is None:
-        X = x
-        Y = y
-      else:
-        X = np.append(X,x,axis=0)
-        Y = np.append(Y,y,axis=0)
-      
-    X = np.array(X)
-    Y = np.array(Y)
+            x = dt.to_recurrent_samples(x, self.window_length)
+            y = np.array(y[self.window_length - 1:])
+            if X is None:
+                X = x
+                Y = y
+            else:
+                X = np.append(X, x, axis=0)
+                Y = np.append(Y, y, axis=0)
 
-    # get the list of indices
-    idx = range(X.shape[0])
-    np.random.shuffle(idx)
+        X = np.array(X)
+        Y = np.array(Y)
 
-    # split into train and test data
-    num_train = int(self.train_test_split * len(idx))
-    train_idx = idx[:num_train]
-    test_idx = idx[num_train:]
+        # get the list of indices
+        idx = range(X.shape[0])
+        np.random.shuffle(idx)
 
-    X_train = X[train_idx]
-    Y_train = Y[train_idx]
-    X_test = X[test_idx]
-    Y_test = Y[test_idx]
+        # split into train and test data
+        num_train = int(self.train_test_split * len(idx))
+        train_idx = idx[:num_train]
+        test_idx = idx[num_train:]
 
-    self.actor.fit(X_train, Y_train,
-        validation_data=(X_test, Y_test),
-        *args, **kwargs)
+        X_train = X[train_idx]
+        Y_train = Y[train_idx]
+        X_test = X[test_idx]
+        Y_test = Y[test_idx]
+
+        self.actor.fit(X_train, Y_train,
+                       validation_data=(X_test, Y_test),
+                       *args, **kwargs)
