6f712fa6cec8356316add2defaaf33a6000c0745
Chris Paxton <cpaxton3@jhu.edu>
2017-03-22 19:52:50 -0700

    adding a quick little simulation node that will provide some fake execution services for tom the robot

---
+++ costar_task_plan/python/costar_task_plan/robotics/tools/tom_sim.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import JointState
+
+# This is a very crude simulator that determines what TOM can do. We use this
+# thing to publish poses where we want the robot to go.
+# It implements the following:
+#  - get planning scene
+#  - joint trajectory cmd
+# And it publishes joint states, too! Wow!
+class TomSim(object):
+
+  def __init__(self):
+    self.seq = 0
+    self.obstacles = []
+
+    # Create the list of joint names for the simple TOM simulator
+    self.joint_names = ['l_front_wheel_joint', 'l_rear_wheel_joint',
+        'r_front_wheel_joint', 'r_rear_wheel_joint', 'r_shoulder_pan_joint',
+        'r_shoulder_lift_joint', 'r_elbow_joint', 'r_wrist_1_joint',
+        'r_wrist_2_joint', 'r_wrist_3_joint', 'l_shoulder_pan_joint',
+        'l_shoulder_lift_joint', 'l_elbow_joint', 'l_wrist_1_joint',
+        'l_wrist_2_joint', 'l_wrist_3_joint', 'r_gripper_left_finger_joint',
+        'r_gripper_left_finger_base_joint', 'r_gripper_right_finger_joint',
+        'r_gripper_right_finger_base_joint', 'r_gripper_mid_finger_joint',
+        'r_gripper_mid_finger_base_joint', 'l_gripper_left_finger_joint',
+        'l_gripper_left_finger_base_joint', 'l_gripper_right_finger_joint',
+        'l_gripper_right_finger_base_joint', 'l_gripper_mid_finger_joint',
+        'l_gripper_mid_finger_base_joint']
+
+    self.default_pose = [0.0, 0.0, 0.0, 0.20294688542190054,
+        -1.0719114121799995, -1.1008140645600006, 1.7366724169200003,
+        -0.8972388608399999, 1.25538042294, -0.028902652380000227,
+        1.2151680370199998, -1.6210618074000003, -2.05585823016,
+        -2.5773626100600002, -1.1008140645600006, -0.8256105484199994,
+        0.0026895523773320003, -0.0006283185307176531, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0]
+
+    self.qs = {}
+    for name, pose in zip(self.joint_names, self.default_pose):
+      self.qs[name] = pose
+
+    self.js_pub = rospy.Publisher('joint_states', JointState, queue_size=1000)
+
+
+  def tick(self):
+    self.seq += 1
+    msg = JointState(name=self.qs.keys(), position=self.qs.values())
+    msg.header.seq = self.seq
+    msg.header.stamp = rospy.Time.now()
+    self.js_pub.publish(msg)
+
+if __name__ == '__main__':
+
+  rospy.init_node('costar_tom_sim')
+  
+  sim = TomSim()
+  rate = rospy.Rate(30)
+  try:
+    while not rospy.is_shutdown():
+      sim.tick()
+      rate.sleep()
+  except rospy.ROSInterruptException, e:
+    pass
