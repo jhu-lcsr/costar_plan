ad38b60903c014d9caa752f4c3c91e9f16adc33d
Chris Paxton <cpaxton3@jhu.edu>
2017-03-23 16:51:42 -0700

    python class wrapping interface

---
+++ costar_planning_interface/python/costar_planning_interface/interface.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+try:
+  from pycostar_planner import CostarPlanner
+except ImportError, e:
+  print "[COSTAR_PLANNING_INTERFACE] Could not create Boost::python bindings!"
+
+# Hack to use roscpp stuff in python
+from moveit_ros_planning_interface._moveit_roscpp_initializer import roscpp_init
+
+roscpp_set = False
+
+# Wrap the C++ planning obbject in something a little more sane.
+class PlanningInterface(object):
+
+    def __init__(self,
+                 robot_description="robot_description",
+                 joint_states_topic="joint_states",
+                 planning_scene_topic="planning_scene",
+                 padding=0.):
+
+        global roscpp_set
+        if not roscpp_set:
+            try:
+                roscpp_init('costar_planning_interface',[])
+            except Exception, e:
+                print e
+
+        # Create the actual interface
+        self.interface = CostarPlanner(
+                robot_description,
+                joint_states_topic,
+                planning_scene_topic,
+                padding)
+
+
+if __name__ == '__main__':
+    pi = PlanningInterface()
+    rospy.spin()
