516f10e94f2299d74de8d385dd74eb94b6626e0e
Chris Paxton <cpaxton3@jhu.edu>
2017-03-23 15:06:41 -0700

    setting up planning and other stuff

--- costar_planning_interface/python/costar_planning_interface/__init__.py
+++ costar_planning_interface/python/costar_planning_interface/__init__.py
@@ -1,5 +1,5 @@
 try:
-    from pycostar_planner import *
+  from pycostar_planner import *
 except ImportError, e:
-print "[COSTAR_PLANNING_INTERFACE] Could not create Boost::python bindings!"
+  print "[COSTAR_PLANNING_INTERFACE] Could not create Boost::python bindings!"
 
