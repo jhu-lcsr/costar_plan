91c47a3ec952b7643e417b7f28630cc2ff9f2814
Chris Paxton <cpaxton3@jhu.edu>
2017-05-04 14:54:14 -0700

    fix issues with assembly sim

--- costar_simulation/scripts/start.py
+++ costar_simulation/scripts/start.py
@@ -5,6 +5,7 @@ Use this script to launch the whole simulation, not the various launch files.
 '''
 
 from gazebo_msgs.srv import SetModelConfiguration
+from std_srvs.srv import Empty as EmptySrv
 
 import rospy
 import signal
@@ -22,19 +23,37 @@ class CostarSimulation(object):
             "wrist_2_joint",
             "wrist_3_joint"]
     joint_positions = [0.30, -1.33, -1.80, -0.27, 1.50, 1.60]
+    experiment = "assembly1"
 
     def __init__(self):
         self.procs = []
+        self.reset_srv = None
 
     def sleep(self, t=1.0):
         time.sleep(t)
 
     def reset(self):
+        self.pause()
         rospy.wait_for_service("gazebo/set_model_configuration")
         configure = rospy.ServiceProxy("gazebo/set_model_configuration", SetModelConfiguration)
         configure(model_name=self.model_name,
                 joint_names=self.joint_names,
                 joint_positions=self.joint_positions)
+        res = subprocess.call([
+            "roslaunch",
+            "costar_simulation",
+            "magnetic_assembly.launch",
+            "experiment:=%s"%self.experiment])
+        self.resume()
+
+    def pause(self):
+        pass
+
+    def resume(self):
+        pass
+
+    def reset_srv_cb(self, msg):
+        self.reset()
 
     def run(self):
 
@@ -64,6 +83,10 @@ class CostarSimulation(object):
         # Start controllers
         roscontrol = subprocess.Popen(['roslaunch', 'costar_simulation', 'controllers.launch'])
         self.procs.append(roscontrol)
+
+        # ---------------------------------------------------------------------
+        # Start reset service
+        self.reset_srv = rospy.Service("reset_simulation", EmptySrv, self.reset_srv_cb)
 
     def shutdown(self):
 
