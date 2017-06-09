edf7b2fa18b1461a774f4a165730975757666d18
Chris Paxton <cpaxton3@jhu.edu>
2017-06-09 05:45:38 -0700

    more indigo install fixes

--- setup/install_indigo.sh
+++ setup/install_indigo.sh
@@ -16,30 +16,23 @@ sudo apt-get install python-scipy python-pygame
 echo "Installing smaller libraries from pip..."
 sudo pip install h5py keras sympy matplotlib pygame gmr networkx dtw pypr gym
 
-echo "======================================================"
-echo "ROS"
-sudo apt-get update -qq
-sudo apt-get install -y python-catkin-pkg python-rosdep python-wstool python-catkin-tools ros-$ROS_DISTRO-catkin python-dev 
-sudo pip install h5py pygame sympy matplotlib pygame gmr networkx tensorflow
-source /opt/ros/$ROS_DISTRO/setup.bash
-sudo rosdep init
-rosdep update
 
 echo "======================================================"
 echo "CATKIN"
 echo "Create catkin workspace..."
 mkdir -p $CATKIN_WS/src
 cd $CATKIN_WS
+source /opt/ros/$ROS_DISTRO/setup.bash
 catkin init
 cd $CATKIN_WS/src
 
 git clone https://github.com/cburbridge/python_pcd.git
-git clone gttps://github.com/jhu-lcsr/costar_objects.git
+git clone https://github.com/jhu-lcsr/costar_objects.git
 git clone https://github.com/cpaxton/dmp.git --branch indigo
 git clone https://github.com/cpaxton/robotiq_85_gripper.git
+git clone https://github.com/cpaxton/costar_plan.git
 rosdep install -y --from-paths ./ --ignore-src --rosdistro $ROS_DISTRO
 cd $CATKIN_WS/src
-source /opt/ros/$ROS_DISTRO/setup.bash
 catkin build
 #source $CATKIN_WS/devel/setup.bash
 
