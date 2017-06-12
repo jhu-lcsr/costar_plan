4933545c4228fd46be2f73753562c9180543b083
alee156 <lealbertle@gmail.com>
2017-06-12 15:51:28 -0400

    Create __init__.py

--- costar_task_plan/python/costar_task_plan/simulation/robots/__init__.py
+++ costar_task_plan/python/costar_task_plan/simulation/robots/__init__.py
@@ -3,6 +3,7 @@
 from abstract import AbstractRobotInterface
 from ur5_robotiq import Ur5RobotiqInterface
 from iiwa_robotiq_3_finger import IiwaRobotiq3FingerInterface
+from jaco_robotiq import JacoRobotiqInterface
 
 __all__ = ["AbstractRobotInterface",
            "Ur5RobotiqInterface",
