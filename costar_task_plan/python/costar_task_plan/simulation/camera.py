02a1c42a272d803fe461d75b424450508cf09a09
Chris Paxton <cpaxton3@jhu.edu>
2017-06-14 15:36:11 -0700

    set up images

--- costar_task_plan/python/costar_task_plan/simulation/camera.py
+++ costar_task_plan/python/costar_task_plan/simulation/camera.py
@@ -15,7 +15,7 @@ class Camera(object):
   image_width: width of image to capture
   '''
 
-  def __init__(self, name, pos, target, up=(0,0,1), image_height=128, image_width=128):
+  def __init__(self, name, pos, target, up=(0,0,1), image_height=128, image_width=128, fov=60, near_plane=0.1, far_plane=1000):
     '''
     Create camera matrix for a particular position in the simulation. Task 
     definitions should produce these and 
@@ -24,6 +24,15 @@ class Camera(object):
     self.matrix = pb.computeViewMatrix(pos, target, up)
     self.image_height = image_height
     self.image_width = image_width
+    self.aspect_ratio = self.image_width / self.image_height
+    self.fov = fov
+    self.near_plane = near_plane
+    self.far_plane = far_plane
+    self.projection_matrix = pb.computeProjectionMatrixFOV(
+            self.fov,
+            self.aspect_ratio,
+            self.near_plane,
+            self.far_plane)
 
   def capture(self):
     return pb.getCameraImage(self.image_width, self.image_height, self.matrix)[2]
