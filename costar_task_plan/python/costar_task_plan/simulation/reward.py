de1e4de4545bab2fe542a194d438c508fa5b7072
Chris Paxton <cpaxton3@jhu.edu>
2017-06-21 15:45:02 -0700

    added a reward function

--- costar_task_plan/python/costar_task_plan/simulation/reward.py
+++ costar_task_plan/python/costar_task_plan/simulation/reward.py
@@ -1,5 +1,9 @@
 from costar_task_plan.abstract import AbstractReward
 
+from tf_conversions import posemath as pm
+
+import numpy as np
+
 class EuclideanReward(AbstractReward):
     '''
     Distance based purely on euclidean distance to object
@@ -11,12 +15,12 @@ class EuclideanReward(AbstractReward):
         '''
         self.goal = goal
 
-    def compute(self, world):
+    def evaluate(self, world):
         '''
         Reward is 0 at object.
         '''
-        robot = world.actors[0]
-        T_ee = pm.fromMatrix(robot.robot.forward(robot.state.arm))
-        T_obj = world.getObject(self.goal).T
-        print -np.linalg.norm(T_obj.p.tolist(), T_ee.p.tolist())
-        return 0
+        robot_actor = world.actors[0]
+        T_ee = pm.fromMatrix(robot_actor.robot.fwd(robot_actor.state.arm))
+        T_obj = world.getObject(self.goal).state.T
+        print -(T_obj.p - T_ee.p).Norm()
+        return 0, 0
