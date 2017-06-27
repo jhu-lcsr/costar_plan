13958c344e683c650d030340697a1b1fb9971431
Chris Paxton <cpaxton3@jhu.edu>
2017-06-26 18:25:19 -0700

    remove unused code

--- costar_task_plan/python/costar_task_plan/models/__init__.py
+++ costar_task_plan/python/costar_task_plan/models/__init__.py
@@ -4,9 +4,6 @@ __all__ = [
     # Machine Learning,
     'GMM', "GmmDynamics",
     # ===========================================================================
-    # Oracles store and manage data
-    'AbstractOracle',
-    # ===========================================================================
     # Adversaries generate worlds
     'AbstractAdversary',
     'RandomAdversary', # choose random params
@@ -21,10 +18,6 @@ from sampler import *
 from fast import *
 from gmm import *
 from gmm_dynamics import *
-
-# saved training data
-from oracle import *
-from policy_oracle import *
 
 # adversary stuff
 from adversary import *
