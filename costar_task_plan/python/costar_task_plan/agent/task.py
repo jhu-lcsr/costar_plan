05924c0595a6377ef962ae6ce033a429c6360e7a
Chris Paxton <cpaxton3@jhu.edu>
2017-07-03 17:32:11 -0700

    adding licenses in various places

--- costar_task_plan/python/costar_task_plan/agent/task.py
+++ costar_task_plan/python/costar_task_plan/agent/task.py
@@ -1,3 +1,38 @@
+'''
+By Chris Paxton
+Copyright (c) 2017, The Johns Hopkins University
+All rights reserved.
+
+This license is for non-commercial use only, and applies to the following
+people associated with schools, universities, and non-profit research institutions
+
+Redistribution and use in source and binary forms by the aforementioned
+people and institutions, with or without modification, are permitted
+provided that the following conditions are met:
+
+* Usage is non-commercial.
+
+* Redistribution should be to the listed entities only.
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+
 from abstract import AbstractAgent
 from costar_task_plan.mcts import ContinuousSamplerTaskPolicies
 from costar_task_plan.mcts import Node, OptionsExecutionManager
