5452fc86cd29574da3a41ca4f18081676b9e657c
Chris Paxton <cpaxton3@jhu.edu>
2017-07-05 17:18:55 -0700

    make sure agents are created properly

--- costar_task_plan/python/costar_task_plan/agent/util.py
+++ costar_task_plan/python/costar_task_plan/agent/util.py
@@ -10,13 +10,15 @@ def GetAgents():
     return ["null", "random", "task", "keras_ddpg", "keras_naf"]
 
 def MakeAgent(env, name, *args, **kwargs):
+    print "============="
+    print name
     try:
         return {
-                'null': NullAgent(env, *args, **kwargs),
-                'random': RandomAgent(env, *args, **kwargs),
-                'task': TaskAgent(env, *args, **kwargs),
-		'keras_ddpg': KerasDDPGAgent(env, *args, **kwargs),
-                }[name]
+                'null': lambda: NullAgent(env, *args, **kwargs),
+                'random': lambda: RandomAgent(env, *args, **kwargs),
+                'task': lambda: TaskAgent(env, *args, **kwargs),
+		'keras_ddpg': lambda: KerasDDPGAgent(env, *args, **kwargs),
+                }[name]()
     except KeyError, e:
         raise NotImplementedError('Agent "%s" not implemented'%name)
 
