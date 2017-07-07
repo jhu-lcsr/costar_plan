5813b8dc99e94397e76ac96e74dba9e2637584e9
Chris Paxton <cpaxton3@jhu.edu>
2017-07-06 18:31:04 -0700

    set up the discriminator network for the GAN

--- costar_task_plan/python/costar_task_plan/models/gan.py
+++ costar_task_plan/python/costar_task_plan/models/gan.py
@@ -1,6 +1,8 @@
 
 import keras.backend as K
+import numpy as np
 
+from keras.layers.advanced_activations import LeakyReLU
 from keras.layers import Input, RepeatVector, Reshape
 from keras.layers import UpSampling2D, Conv2DTranspose
 from keras.layers import BatchNormalization
@@ -9,23 +11,30 @@ from keras.layers.merge import Concatenate
 from keras.models import Model
 
 class GAN(object):
-    def __init__(self, inputs, generator, discriminator, batch_size):
+    def __init__(self, generator, discriminator, noise_dim):
         self.generator = generator
         self.discriminator = discriminator
-        self.batch_size = batch_size
+        self.noise_dim = noise_dim
 
-        self.adversarial_model = discriminator(generator)
+        self.printSummary()
 
-    def summary(self):
-        print generator.summary()
-        print discriminator.summary()
+        #self.adversarial_model = discriminator(generator)
 
-    def fit(self, x, y, num_iter=1000, save_interval=0):
-        #idx = np.random.randint(0,
-        #    self.x_train.shape[0], size=self.batch_size)
-        #x = self.x_train[idx]
-        #y = self.y_train[idx]
-        pass
+    def printSummary(self):
+        print self.generator.summary()
+        print self.discriminator.summary()
+
+    def fit(self, x, y, num_iter=1000, batch_size=50, save_interval=0):
+        for i in xrange(num_iter):
+
+            # Sample one batch, including random noise
+            idx = np.random.randint(0, y.shape[0], size=batch_size)
+            xi = x[idx]
+            yi = y[idx]
+            noise = np.random.random((batch_size, self.noise_dim))
+
+            #loss = self.generator.train_on_batch([noise, yi], xi)
+            #print "Iter %d: loss = %f"%(i,loss)
 
 class SimpleGAN(GAN):
     '''
@@ -47,7 +56,7 @@ class SimpleImageGan(GAN):
     '''
 
     def __init__(self, img_rows=28, img_cols=28, channels=1, labels=10,
-            noise_dim=100, batch_size=100,):
+            noise_dim=100,):
         self.img_shape = (img_rows, img_cols, channels)
 
         self.generator_dense_size = 1024
@@ -56,17 +65,18 @@ class SimpleImageGan(GAN):
         self.discriminator_dense_size = 1024
         self.discriminator_filters_c1 = 64
 
-        self.batch_size = batch_size
-
         generator = self._generator(self.img_shape, labels, noise_dim)
+        discriminator = self._discriminator(self.img_shape, labels)
         generator.compile(optimizer="adam",loss="binary_crossentropy")
-        print generator.summary()
+        discriminator.compile(optimizer="adam",loss="binary_crossentropy")
+
+        super(SimpleImageGan, self).__init__(generator, discriminator, noise_dim)
 
     def _generator(self, img_shape, num_labels, noise_dim):
         height4 = img_shape[0]/4
-        width4 = img_shape[0]/4
+        width4 = img_shape[1]/4
         height2 = img_shape[0]/2
-        width2 = img_shape[0]/2
+        width2 = img_shape[1]/2
 
         labels = Input(shape=(num_labels,))
         noise = Input(shape=(noise_dim,))
@@ -113,3 +123,54 @@ class SimpleImageGan(GAN):
         x = Activation('relu')(x)
         
         return Model([noise, labels], x)
+
+    def _discriminator(self, img_shape, num_labels):
+        # just for my sanity
+        height4 = img_shape[0]/4
+        width4 = img_shape[1]/4
+        height2 = img_shape[0]/2
+        width2 = img_shape[1]/2
+        height = img_shape[0]
+        width = img_shape[1]
+        channels = img_shape[2]
+
+        # sampled images
+        samples = Input(shape=img_shape)
+
+        # labels created, copied, and reshaped
+        labels = Input(shape=(num_labels,))
+        labels2 = RepeatVector(height*width)(labels)
+        labels2 = Reshape((height,width,num_labels))(labels2)
+
+        # Add initial discriminator layer
+        x = Concatenate(axis=3)([samples, labels2])
+        x = Conv2D(channels + num_labels,
+                   kernel_size=[5, 5], 
+                   strides=(2, 2),
+                   padding="same")(x)
+        x = LeakyReLU(alpha=0.2)(x)
+
+        # Add conv layer with more filters
+        labels2 = RepeatVector(height2*width2)(labels)
+        labels2 = Reshape((height2,width2,num_labels))(labels2)
+        x = Concatenate(axis=3)([x, labels2])
+        x = Conv2D(self.discriminator_filters_c1 + num_labels,
+                   kernel_size=[5, 5], 
+                   strides=(2, 2),
+                   padding="same")(x)
+        x = BatchNormalization(momentum=0.9, epsilon=1e-5)(x)
+        x = LeakyReLU(alpha=0.2)(x)
+
+        # Add dense layer
+        x = Reshape((-1,))(x)
+        x = Concatenate(axis=1)([x, labels])
+        x = Dense(self.discriminator_dense_size)(x)
+        x = BatchNormalization(momentum=0.9, epsilon=1e-5)(x)
+        x = LeakyReLU(alpha=0.2)(x)
+
+        # Single output -- sigmoid activation function
+        x = Concatenate(axis=1)([x, labels])
+        x = Dense(1)(x)
+        x = Activation('sigmoid')(x)
+
+        return Model([samples, labels], x)
