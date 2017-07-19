bc47c310d348cbfdcb675ed717b2b6a2f3addca1
alee156 <lealbertle@gmail.com>
2017-07-19 11:53:53 -0400

    Create albert.py

---
+++ costar_task_plan/python/costar_task_plan/agent/albert.py
@@ -0,0 +1,26 @@
+from abstract import AbstractAgent
+import pybullet as pb
+
+class RandomAgent(AbstractAgent):
+    '''
+    Really simple test agent that just generates a random set of positions to
+    move to.
+    '''
+
+    name = "albert"
+
+    def __init__(self, env, *args, **kwargs):
+        super(RandomAgent, self).__init__(*args, **kwargs)
+        self.env = env
+
+    def fit(self, num_iter):
+        a = pb.getKeyboardEvents()
+        while a != ['a']:
+
+
+            print a
+
+            #self.env.step(cmd)
+
+            if self._break:
+                return
