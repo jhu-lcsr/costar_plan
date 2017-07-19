856d63a5240f4875b46a938d43460b75329e45c6
alee156 <lealbertle@gmail.com>
2017-07-19 14:12:35 -0400

    Update albert.py

--- costar_task_plan/python/costar_task_plan/agent/albert.py
+++ costar_task_plan/python/costar_task_plan/agent/albert.py
@@ -1,5 +1,6 @@
 from abstract import AbstractAgent
 import pybullet as pb
+import PyKDL as kdl
 
 class AlbertAgent(AbstractAgent):
     '''
@@ -15,10 +16,48 @@ class AlbertAgent(AbstractAgent):
 
     def fit(self, num_iter):
         a = pb.getKeyboardEvents()
-        while a != ['a']:
 
+        for i in xrange(num_iter):
+            print "---- Iteration %d ----"%(i+1)
+            self.env.reset()
 
-            print a
+            while not self._break:
+                state = self.env.world.actors[0].state
+                control = None
+                a = pb.getKeyboardEvents()
+                
+                # y opens the gripper
+                if 121 in a:  
+                    # arm = 
+                    gripper_cmd = state.robot.gripperOpenCommand()
+                    control = SimulationRobotAction(arm_cmd=None, gripper_cmd=gripper_cmd)
+                    
+                elif 122 in a:  
+                    # arm = 
+                    gripper_cmd = state.robot.gripperCloseCommand()
+                    control = SimulationRobotAction(arm_cmd=None, gripper_cmd=gripper_cmd)
+                    
+                if 119 in a:  
+                    origin = state.T
+                    move = kdl.Frame(kdl.Rotation.Identity(), kdl.Vector(0,0,0))
+                    arm = origin * move
+                    gripper_cmd = None
+                    invarm = state.robot.ik(T_step, state.arm)
+                    control = SimulationRobotAction(arm_cmd=invarm, gripper_cmd=gripper_cmd)
+
+                if control is not None:
+                    features, reward, done, info = self.env.step(control)
+                    self._addToDataset(self.env.world,
+                            control,
+                            features,
+                            reward,
+                            done,
+                            i,
+                            names[plan.idx])
+                    if done:
+                        break
+                else:
+                    break
 
             #self.env.step(cmd)
 
