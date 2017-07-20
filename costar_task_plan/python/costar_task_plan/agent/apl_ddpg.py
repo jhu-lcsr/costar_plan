ca7b0588fa163cac128c2dac542e91875b062eb9
Kapil Katyal <kdk132@gmail.com>
2017-07-20 11:47:59 -0400

    modifying reward function

--- costar_task_plan/python/costar_task_plan/agent/apl_ddpg.py
+++ costar_task_plan/python/costar_task_plan/agent/apl_ddpg.py
@@ -91,7 +91,7 @@ class APLDDPGAgent(AbstractAgent):
         self.observation = env.reset()
         self.state_dim = self.observation.shape
         print ">>>>>>>>>>>>>>>>>>>>>state dim " + str(self.state_dim)
-        self.nn_action_dim = 2 # limit ddpg network output to 3 DOF
+        self.nn_action_dim = 6 # limit ddpg network output to 3 DOF
         self.noise = OUProcess(self.nn_action_dim, mu=OU_MEAN, theta=OU_THETA, sigma=EPSILON_RANGE[0])
         
         
