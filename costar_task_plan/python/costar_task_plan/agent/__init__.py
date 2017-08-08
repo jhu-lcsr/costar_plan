109ca0697b4ea6b57e683f8c0b1ba2fc6c464ad8
alee156 <lealbertle@gmail.com>
2017-08-08 15:57:51 -0400

    Update __init__.py

--- costar_task_plan/python/costar_task_plan/agent/__init__.py
+++ costar_task_plan/python/costar_task_plan/agent/__init__.py
@@ -7,12 +7,14 @@ from random import *
 from task import *
 from albert import *
 from albert2 import *
+from albert3 import *
 
 from util import *
 
 __all__ = [
         # ============================
         "Albert2Agent",
+        "Albert3Agent"
         "AlbertAgent",
         "RandomAgent",
         "NullAgent",
