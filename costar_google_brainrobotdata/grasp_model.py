7fe7c0db82da27c6ebef6019a26af4a52a6b0827
Andrew Hundt <ATHundt@gmail.com>
2017-08-23 03:01:00 -0400

    INTERMEDIATE CODE: more attempts at tiling grasp_model.py

--- costar_google_brainrobotdata/grasp_model.py
+++ costar_google_brainrobotdata/grasp_model.py
@@ -3,16 +3,97 @@ from keras.applications.resnet50 import ResNet50
 from keras import backend as K
 from keras.layers.core import Dense
 from keras.layers.core import Flatten
+from keras.layers.core import RepeatVector
 from keras.layers import Input
 from keras.layers.merge import concatenate
 from keras.layers.merge import Concatenate
+from keras.layers.merge import _Merge
 from keras.models import Model
+from keras.layers import Lambda
+from keras.layers import Reshape
+from keras.applications.imagenet_utils import _obtain_input_shape
 
+
+# def SingleChannelToImage(channels, image_shape):
+#     if(K.image_data_format() == 'channels_first'):
+#         x = Lambda(lambda x: K.tile(x, tuple(1) + K.shape(image_shape[1:]))(channels)
+#     elif(K.image_data_format() == 'channels_last'):
+#         x = Lambda(lambda x: K.tile(x, image_shape[:-1] + tuple(1)))(channels)
+#     return x
+def repeat_vector_as_image(channels_image):
+    channels, image = channels_image
+    image_shape = K.shape(image)
+    shape_to_tile = image_shape[:-1] + [1]
+    single_pixel_image_shape = [1, 1] + K.shape(channels)
+    single_pixel_image = K.reshape(channels, single_pixel_image_shape)
+    return K.tile(single_pixel_image, shape_to_tile)
+
+def repeat_vector_as_image2(vector, image):
+    if K.image_data_format() == 'channels_first':
+        concat_axis = 1
+        im_channel, im_row, im_col = K.shape(image)
+        v_channel = K.shape(vector)
+    else:
+        concat_axis = -1
+        im_row, im_col, im_channel = K.shape(image)
+        v_channel = K.shape(vector)
+
+    x = Lambda(lambda x: K.tile(x, im_row * im_col))(vector)
+    x = Reshape((im_row, im_col, v_channel))(x)
+
+def repeat_vector_as_image3(vector, image_shape):
+    if K.image_data_format() == 'channels_first':
+        concat_axis = 1
+        im_channel, im_row, im_col = image_shape
+        v_channel = K.shape(vector)
+    else:
+        concat_axis = -1
+        im_row, im_col, im_channel = image_shape
+        v_channel = K.shape(vector)
+
+    x = RepeatVector(im_row * im_col)(vector)
+    x = Reshape((int(image_shape[0]), int(image_shape[1]), v_channel))(x)
+    x = K.reshape((image_shape[0], image_shape[1], v_channel))(x)
+
+def repeat_vector_as_image4(vector, logits):
+
+    # Determine proper input shape
+    image_shape = _obtain_input_shape(logits.shape[1:],  # current_time_resnet50.output.shape[1:],
+                                      default_size=32,
+                                      min_size=1,
+                                      require_flatten=False,
+                                      data_format=K.image_data_format())
+    if K.image_data_format() == 'channels_first':
+        concat_axis = 1
+        im_channel, im_row, im_col = image_shape
+        v_channel = K.shape(vector)
+    else:
+        concat_axis = -1
+        row_axis = 1
+        col_axis = 2
+        im_row, im_col, im_channel = image_shape
+        v_channel = K.shape(vector)
+
+    x = RepeatVector(im_row * im_col)(vector)
+    x = Lambda(lambda y: K.reshape(y, logits.shape[row_axis] + logits.shape[col_axis] + v_channel[concat_axis]))(x)
 
 def grasp_model(clear_view_image_op, current_time_image_op, pose_op):
     print('clear_view_image_op.shape: ', clear_view_image_op.shape)
+    # TODO(ahundt) clear view only changes very occasionally compared to others, save output as variable.
     clear_view_resnet50 = ResNet50(include_top=False, input_tensor=clear_view_image_op)
     current_time_resnet50 = ResNet50(include_top=False, input_tensor=current_time_image_op)
+    vect_input = Input(tensor=pose_op)
+    clear_view_logits = Input(tensor=clear_view_resnet50.output)
+    current_time_logits = Input(tensor=current_time_resnet50.output)
+
+    # vec_image = Lambda(repeat_vector_as_image)([pose_op, current_time_resnet50.output])
+    # vec_image = repeat_vector_as_image2(vect_input, current_time_resnet50.output)
+
+    print(current_time_resnet50.output.shape)
+    print(current_time_resnet50.output)
+    print('clear_view_logits.shape: ', clear_view_logits.shape)
+    print('clear_view_logits._keras_shape:', clear_view_logits._keras_shape)
+    vec_image = repeat_vector_as_image4(vect_input, clear_view_logits)
 
     # TODO(ahundt) concatenate pose op at every pixel
     # pose_tensor = Input(tensor=pose_op)
@@ -21,7 +102,10 @@ def grasp_model(clear_view_image_op, current_time_image_op, pose_op):
     # tiled_pose_filter = K.tile(pose_filter, clear_view_resnet50.output.shape)
     # tiled_pose_filter = K.reshape(tiled_pose_filter, (clear_view.shape[0], clear_view.shape[1], pose_filter.size))
     concat_axis = 1 if K.image_data_format() == 'channels_first' else -1
-    fused_data = Concatenate(concat_axis)([clear_view_resnet50.output, current_time_resnet50.output])
+
+    fused_data = Concatenate(concat_axis)([clear_view_resnet50.output,
+                                           vec_image,
+                                           current_time_resnet50.output])
 
     x = ResNet50(fused_data, weights=None)
 
