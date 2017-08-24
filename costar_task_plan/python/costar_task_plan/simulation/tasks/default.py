4f3b998c7fe9367f85e0d51e4298da2b3b509b00
Chris Paxton <cpaxton3@jhu.edu>
2017-08-23 19:26:27 -0700

    randomize arm starting position

--- costar_task_plan/python/costar_task_plan/simulation/tasks/default.py
+++ costar_task_plan/python/costar_task_plan/simulation/tasks/default.py
@@ -11,7 +11,8 @@ import pybullet as pb
 class DefaultTaskDefinition(AbstractTaskDefinition):
 
     # These are for the UR5
-    joint_positions = [0.30, -1.33, -1.80, -0.27, 1.50, 1.60]
+    joint_positions = np.array([0.30, -1.33, -1.80, -0.27, 1.50, 1.60])
+    random_limit = 0.8
 
     # define folder for blocks
     urdf_dir = "urdf"
@@ -31,8 +32,11 @@ class DefaultTaskDefinition(AbstractTaskDefinition):
                 fov=40))
 
     def _setupRobot(self, handle):
-        self.robot.place([0, 0, 0], [0, 0, 0, 1], self.joint_positions)
-        self.robot.arm(self.joint_positions, pb.POSITION_CONTROL)
+        q = self.joint_positions + \
+            (self.random_limit*np.random.random(self.joint_positions.shape) - 
+             self.random_limit*0.5)
+        self.robot.place([0, 0, 0], [0, 0, 0, 1], q)
+        self.robot.arm(q, pb.POSITION_CONTROL)
         self.robot.gripper(self.robot.gripperOpenCommand(), pb.POSITION_CONTROL)
 
     def reset(self):
