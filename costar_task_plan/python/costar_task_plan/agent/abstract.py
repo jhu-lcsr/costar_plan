b0394c178c9c5d9da826e7deaa60b299d7c0e4d0
Andrew Hundt <ATHundt@gmail.com>
2017-08-25 18:17:02 -0400

    Created simple and clean TFRecordConverter

--- costar_task_plan/python/costar_task_plan/agent/abstract.py
+++ costar_task_plan/python/costar_task_plan/agent/abstract.py
@@ -32,126 +32,11 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
-
 import tensorflow as tf
 import numpy as np
 import os
 import signal
-
-
-def _tfrecord_value_error(value):
-            raise ValueError('Attempting to write unsupported data type {}, '
-                             'add support for the new feature type '
-                             'or update your code to write the data in a '
-                             'supported format such as'
-                             ' numpy arrays.'.format(str(type(value))))
-
-
-def _tfrecord_dtype_feature(value):
-    """Create lambda functions matched to the appropriate
-       tf.train.Feature class for the dtype of ndarray or integer value.
-
-       The purpose of this function is for when you are writing the same tfrecord
-       features over and over the expensive type checks do not need to be performed
-       over and over because you can save these lambda functions that will generate
-       the exact type you need for that feature.
-       reference: https://gist.github.com/swyoon/8185b3dcf08ec728fb22b99016dd533f
-
-       It is important to note that the numpy arrays you write must be 1 dimensional,
-       so first write a shape array, then write the result of `array.flatten()`.
-       This will ensure you write 2 1d arrays that together give you all the information
-       you need to reconstruct your original nd array.
-    """
-    if type(value).__module__ == np.__name__:
-        dtype_ = value.dtype
-        if dtype_ == np.float64 or dtype_ == np.float32:
-            return lambda array: tf.train.Feature(float_list=tf.train.FloatList(value=array))
-        elif dtype_ == np.int64 or dtype_ == np.int32:
-            return lambda array: tf.train.Feature(int64_list=tf.train.Int64List(value=array))
-        elif dtype_ == np.uint8 or dtype_ == np.int8:
-            return lambda array: tf.train.Feature(bytes_list=tf.train.BytesList(value=array))
-        else:
-            _tfrecord_value_error(value)
-    elif isinstance(value, int):
-        return lambda array: tf.train.Feature(int64_list=tf.train.Int64List(value=array))
-    elif isinstance(value, float):
-        return lambda array: tf.train.Feature(float_list=tf.train.FloatList(value=array))
-    else:
-        _tfrecord_value_error(value)
-
-
-def _tfrecord_dtype_lambda_dict(dict):
-    """
-    Create a dictionary of lambda function dictionaries, so type checks
-    don't have to be re-done every time data is saved. Sorry this is a bit tricky,
-    but we have to write out both the array and its shape when writing ndarrrays,
-    so there must be two entries added for each single numpy ndarray.
-
-    Here is a reference for updating dicts that might help with what's going on:
-    https://stackoverflow.com/a/45043651/99379
-
-    # Returns
-
-        A dict containing the entries like following:
-        {
-            key: lambda_function(key, value)
-        }
-
-
-        Later if you have a dict with keys that are feature strings and values that are ndarrays,
-        you can call dict[key](value). This will initialize the tf.train.Feature object you need
-        to write out to a tfrecords file. See _tfrecord_write_example for a demonstration of usage.
-    """
-    features = {}
-    class tfrecord_dtypes:
-        # maintain lambdas for lifetime of
-        # writing records, written as nonlocal
-        # class definition for python 2.7 compatibility
-        feature_name_to_lambda = {}
-    for key, value in dict:
-        if type(value).__module__ == np.__name__:
-            # reference for calling the lambdas then saving them internally
-            # https://stackoverflow.com/a/10452819/99379
-            tfrecord_dtypes.feature_name_to_lambda[key] = _tfrecord_dtype_feature(value)
-            value_shape = np.array(value.shape, dtype=np.int64)
-            tfrecord_dtypes.feature_name_to_lambda[key + '_shape'] = _tfrecord_dtype_feature(value)
-            key_shape_lambda = _tfrecord_dtype_feature(value_shape)
-            features[key] = lambda fkey, array: {
-                fkey + '_shape': tfrecord_dtypes.feature_name_to_lambda[fkey + '_shape'](
-                    np.array(array.shape, dtype=np.int64)),
-                fkey: tfrecord_dtypes.feature_name_to_lambda[fkey](array.flatten())
-            }
-        elif isinstance(value, (int, float)):
-            # reference for calling the lambdas then saving them internally
-            # https://stackoverflow.com/a/10452819/99379
-            key_feature_lambda = _tfrecord_dtype_feature(value)
-            features[key] = lambda fkey, i: {fkey: key_feature_lambda(i)}
-        else:
-            _tfrecord_value_error(value)
-
-    return features
-
-
-def _tfrecord_write_example(writer, dtype_lambda_dict, data_to_write):
-    """Write an example to a file
-    See also: _tfrecord_dtype_lambda_dict
-    """
-    tf_train_features_dict = {}
-    for key, value in data_to_write:
-        # get the function that generates the feature,
-        # call it, and update the feature dictionary
-        # with the newly created feature object
-        #
-        # This is a bit complicated because with ndarrays
-        # we want to create both an entry with 'feature_name'
-        # and 'feature_name_shape', so the data can be easily
-        # extracted later.
-        tf_train_features_dict.update(dtype_lambda_dict[key](key, value))
-
-    features = tf.train.Features(feature=tf_train_features_dict)
-    example = tf.train.Example(features=features)
-    serialized = example.SerializeToString()
-    writer.write(serialized)
+from costar_models.datasets.tfrecord import TFRecordConverter
 
 
 class AbstractAgent(object):
@@ -213,7 +98,7 @@ class AbstractAgent(object):
         if self.data_type == self.NUMPY_ZIP:
             self.data = {}
         else:
-            self.tf_writer = tf.python_io.TFRecordWriter(data_file)
+            self.tf_writer = TFRecordConverter(data_file)
 
         self.datafile_name = data_file
         self.datafile = os.path.join(directory, data_file)
@@ -293,10 +178,9 @@ class AbstractAgent(object):
         written out to a numpy archive.
         '''
         if self.data_type == self.TFRECORD:
-            if self.tfrecord_lambda_dict is None:
-                self.tfrecord_lambda_dict = _tfrecord_dtype_lambda_dict(data)
-
-            _tfrecord_write_example(self.tf_writer, self.tfrecord_lambda_dict, data)
+            if self.tf_writer.ready_to_write() is False:
+                self.tf_writer.prepare_to_write(data)
+            self.tf_writer.write_example(data)
 
         elif self.data_type == self.NUMPY_ZIP:
             for key, value in data:
