2e55b6836fd20263f4301d4f9b0187606700d391
Chris Paxton <cpaxton3@jhu.edu>
2017-08-28 12:32:56 -0700

    cleanup and comments

--- costar_models/python/costar_models/util.py
+++ costar_models/python/costar_models/util.py
@@ -9,8 +9,7 @@ from multi_trajectory_sampler import RobotMultiTrajectorySampler
 from multi_autoencoder_model import RobotMultiAutoencoder
 from multi_hierarchical import RobotMultiHierarchical
 
-from multi_unsupervised_model import RobotMultiUnsupervised
-from multi_unsupervised1_model import RobotMultiUnsupervised1
+# Model for sampling predictiosn
 from multi_sampler import RobotMultiPredictionSampler
 
 def MakeModel(features, model, taskdef, **kwargs):
