47ea83af217ab3bcb58392abeb263fac11b64525
Chris Paxton <cpaxton3@jhu.edu@login-node02.cm.cluster>
2017-08-29 14:15:17 -0400

    update setup scripts for marcc + models

---
+++ setup/costar_models_module/0.0.1.lua
@@ -0,0 +1,4 @@
+whatis([[local module for costar_models ]])
+
+prepend_path{"PATH","$HOME/.local/bin",delim=":",priority="0"}
+append_path{"PYTHONPATH", "/$HOME/.local/lib/python2.7/site-packages", delim=":",priority="0"}
--- setup/setup_marcc.sh
+++ setup/setup_marcc.sh
@@ -12,4 +12,21 @@ git clone git@github.com:cpaxton/costar_plan.git
 pip install --user h5py keras keras-rl sympy matplotlib gmr networkx \
   dtw pypr gym PyPNG pybullet numba
 
+# -----------------------------------------------------------
+# Unused, but retained as an example:
+#export CTP_MODELS=$HOME/costar_plan/costar_models/python
+#cd $CTP_MODELS
+#python setup.py install --prefix=$HOME/my-python-modules 
+# Default path:
+# $HOME/.local/lib/python3.6/site-packages/$PATH
+
+# -----------------------------------------------------------
+# For now we have a constant location where we create these files
+export CTP_MODELS=$HOME/costar_plan/costar_models/python
+cd $CTP_MODELS
+python setup.py install --prefix=$HOME/my-python-modules 
+cd $HOME
+
+mkdir -p $HOME/privatemodules/
+ln -s $HOME/costar_plan/setup/costar_models_module $HOME/privatemodules/costar_models
 
