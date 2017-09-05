3e6691d4da933ae3c3ce6fefbd8fa00bc2dfe85c
Andrew Hundt <ATHundt@gmail.com>
2017-09-05 17:44:31 -0400

    Revert "grasp_train.py remove (hopefully) redundant parse flags line"
    This reverts commit 85bb05790c1b691d24b03a17b1e7717bbd466f26.

--- costar_google_brainrobotdata/grasp_train.py
+++ costar_google_brainrobotdata/grasp_train.py
@@ -51,6 +51,7 @@ flags.DEFINE_integer('densenet_dense_blocks', 4,
 flags.DEFINE_float('densenet_reduction', 0.5,
                    """DenseNet and DenseNetFCN reduction aka compression parameter.""")
 
+flags.FLAGS._parse_flags()
 FLAGS = flags.FLAGS
 
 
