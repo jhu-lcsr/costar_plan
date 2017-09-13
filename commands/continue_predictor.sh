c14e73002fb038174226c8406b02c025206f5ed7
Andrew Hundt <ATHundt@gmail.com>
2017-09-13 19:44:03 -0400

    update continue

--- commands/continue_predictor.sh
+++ commands/continue_predictor.sh
@@ -3,5 +3,6 @@ rosrun costar_models ctp_model_tool \
 	--features multi -i 50000 -e 100 \
 	--model predictor --data_file ~/datasets/ctp_small.npz \
 	--si 0 --lr 0.001 --optimizer nadam \
+        --batch_size 64 \
 	--load_model
 
