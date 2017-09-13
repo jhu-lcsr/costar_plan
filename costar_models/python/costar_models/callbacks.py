2ef121a9d5f777c3dd7ae15a03afdea2825db834
Andrew Hundt <ATHundt@gmail.com>
2017-09-13 16:12:44 -0400

    fixing some issues with scripts, ctp tools on femur

--- costar_models/python/costar_models/callbacks.py
+++ costar_models/python/costar_models/callbacks.py
@@ -44,6 +44,7 @@ class PredictorShowImage(keras.callbacks.Callback):
         img = self.targets[0][:,:imglen]
         img = np.reshape(img, (self.num,64,64,3))
         data, arms, grippers, label = self.predictor.predict(self.features)
+	plt.ioff()
         if self.verbose:
             print("============================")
         for j in range(self.num):
