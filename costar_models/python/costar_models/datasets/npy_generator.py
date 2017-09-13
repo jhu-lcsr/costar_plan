5d13ac24d78cf14dcf7e8b304139d0f6edb188de
Chris Paxton <cpaxton3@jhu.edu>
2017-09-13 14:21:24 -0700

    early commit for new dataset

---
+++ costar_models/python/costar_models/datasets/npy_generator.py
@@ -0,0 +1,50 @@
+
+
+class NpzGeneratorDataset(object):
+    '''
+    Get the list of objects from a folder full of NP arrays. 
+    '''
+
+    def __init__(self, name, split=0.1, ):
+        '''
+        Set name of directory
+        '''
+        self.name = name 
+        self.split = split
+        self.train = []
+        self.test = []
+
+    def write(self, *args, **kwargs):
+        raise NotImplementedError('this dataset does not save things')
+
+
+    def load(self, success_only=False):
+        '''
+        Read the file; get the list of acceptable entries; split into train and
+        test sets.
+        '''
+        files = os.listdir(self.name)
+        files.sort()
+        sample = {}
+        i = 0
+        acceptable_files = []
+        for f in files:
+            if not f[0] == '.':
+                print("%d:"%i+1, f)
+                if success_only:
+                    name = f.split('.')
+                    if name[0] == 'failure':
+                        continue
+                if i == 0:
+                    fsample = np.load(os.path.join(self.name,f))
+                    for key, value in fsample.items():
+                        if key not in sample:
+                            sample[key] = value
+                        if value.shape[0] == 0:
+                            continue
+                        sample[key] = np.concatenate([sample[key],value],axis=0)
+                i += 1
+                acceptable_files.append(f)
+
+        idx = np.array(range(len(acceptable_files)))
+        
