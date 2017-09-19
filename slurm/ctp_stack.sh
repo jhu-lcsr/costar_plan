ba5962ef9157607350285bc9ebc51377b9e68cbf
Chris Paxton <cpaxton3@jhu.edu>
2017-09-19 17:20:25 -0400

    add batch size and clean up the script

--- slurm/ctp_stack.sh
+++ slurm/ctp_stack.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=0-48:0:0
 #SBATCH --nodes=1
 #SBATCH -p unlimited
-#SBATCH -g 1
+#SBATCH -g 2
 #SBATCH --cpus-per-task=6
 #SBATCH --mail-type=end
 #SBATCH --mail-user=cpaxton3@jhu.edu
@@ -16,5 +16,13 @@ echo
 echo "Running $@ on $SLURMD_NODENAME ..."
 echo
 
-$HOME/costar_plan/costar_models/scripts/ctp_model_tool --features multi -e 100 --model predictor --data_file $HOME/work/ctp_test2.npz --lr 0.001  --model_directory $HOME/.costar/models_stack/ --optimizer adam
+$HOME/costar_plan/costar_models/scripts/ctp_model_tool \
+	--features multi \
+	-e 100 \
+	--model predictor \
+	--data_file $HOME/work/ctp_test2.npz \
+	--lr 0.001 \
+	 --model_directory $HOME/.costar/models_stack/ \
+	--optimizer adam \
+	--batch_size 64
 
