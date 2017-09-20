8041542cddbc0f5bdb8395415bf4ab2675b1e7c1
Kapil Katyal <kdk132@gmail.com>
2017-09-20 18:04:52 -0400

    integrate network architecture changes by cpaxton with husky robot

--- costar_models/python/costar_models/husky_callbacks.py
+++ costar_models/python/costar_models/husky_callbacks.py
@@ -5,14 +5,18 @@ import keras
 import matplotlib.pyplot as plt
 import numpy as np
 
+DEFAULT_MODEL_DIRECTORY = os.path.expanduser('~/.costar/models')
+
+
 class PredictorShowImage(keras.callbacks.Callback):
     '''
     Save an image showing what some number of frames and associated predictions
     will look like at the end of an epoch.
     '''
-    directory = os.path.expanduser("~/.costar/debug")
+    
 
     def __init__(self, predictor, features, targets,
+            model_directory=DEFAULT_MODEL_DIRECTORY,
             num_hypotheses=4,
             verbose=False,
             min_idx=0, max_idx=66, step=11):
@@ -31,9 +35,10 @@ class PredictorShowImage(keras.callbacks.Callback):
         self.idxs = range(min_idx, max_idx, step)
         self.num = len(self.idxs)
         self.features = [f[self.idxs] for f in features]
-        self.targets = [t[self.idxs] for t in targets]
+        self.targets = [np.squeeze(t[self.idxs]) for t in targets]
         self.epoch = 0
         self.num_hypotheses = num_hypotheses
+        self.directory = os.path.join(model_directory,'debug')
         if not os.path.exists(self.directory):
             os.makedirs(self.directory)
 
@@ -41,12 +46,23 @@ class PredictorShowImage(keras.callbacks.Callback):
         # take the model and print it out
         self.epoch += 1
         imglen = 64*64*3
-        img = self.targets[0][:,:imglen]
+        #img = self.targets[0][:,:imglen]
+        if len(self.targets[0].shape) == 2:
+          img = self.targets[0][:,:imglen]
+        elif len(self.targets[0].shape) == 3:
+            assert self.targets[0].shape[1] == 1
+            img = self.targets[0][:,0,:imglen]
+        else:
+            raise RuntimeError('did not recognize big train target shape; '
+                               'are you sure you meant to use this callback'
+                               'and not a normal image callback?')
         img = np.reshape(img, (self.num,64,64,3))
-        print (len(self.features))
+        
         data, poses, label = self.predictor.predict(self.features)
         if self.verbose:
             print("============================")
+
+
         for j in range(self.num):
             name = os.path.join(self.directory,
                     "predictor_epoch%d_result%d.png"%(self.epoch,j))
@@ -62,14 +78,16 @@ class PredictorShowImage(keras.callbacks.Callback):
             plt.imshow(img[j])
             for i in range(self.num_hypotheses):
                 if self.verbose:
-                    print("Poses = ", poses[j][i])
-                    
+                    print("Pose = ", poses[j][i])
                     print("Label = ", np.argmax(label[j][i]))
                 plt.subplot(1,2+self.num_hypotheses,i+2)
                 plt.imshow(np.squeeze(data[j][i]))
                 plt.title('Hypothesis %d'%(i+1))
             fig.savefig(name, bbox_inches="tight")
             if self.verbose:
-                print("Pose target = ", self.targets[0][j,imglen:imglen+7])
-                print("Label target = ", np.argmax(self.targets[0][j,(imglen+7):]))
+                print("Arm/gripper target = ",
+                        self.targets[0][j,imglen:imglen+6])
+                print("Label target = ",
+                        np.argmax(self.targets[0][j,(imglen+6):]))
             plt.close(fig)
+
