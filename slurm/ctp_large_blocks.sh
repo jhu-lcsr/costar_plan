43494280a011a0d07c8cc5ccbbec010dd0880d6c
Chris Paxton <cpaxton3@jhu.edu>
2017-09-27 12:08:31 -0400

    update GPU info

--- slurm/ctp_large_blocks.sh
+++ slurm/ctp_large_blocks.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 #SBATCH --job-name=ctp_large_blocks
 #SBATCH --time=0-48:0:0
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
 #SBATCH --nodes=1
-#SBATCH -p unlimited
-#SBATCH -g 1
-#SBATCH --cpus-per-task=6
+#SBATCH --mem=8G
 #SBATCH --mail-type=end
 #SBATCH --mail-user=cpaxton3@jhu.edu
 
@@ -16,5 +16,14 @@ echo
 echo "Running $@ on $SLURMD_NODENAME ..."
 echo
 
-$HOME/costar_plan/costar_models/scripts/ctp_model_tool --features multi -e 100 --model predictor --data_file $HOME/work/ctp_large_v2.npz --lr 0.001  --model_directory $HOME/.costar/models_large/ --optimizer nadam
+module load tensorflow/cuda-8.0/r1.3 
+
+$HOME/costar_plan/costar_models/scripts/ctp_model_tool \
+	--features multi \
+	-e 100 --model predictor \
+	--data_file $HOME/work/ctp_large_v2.npz \
+	--lr 0.001 \
+	 --model_directory $HOME/.costar/models_large/ \
+	--optimizer adam \
+	--batch_size 64 
 
