d357408c4385386f5d8a6804a945eb5f140f43cb
Chris Paxton <cpaxton3@jhu.edu>
2017-09-29 20:27:58 -0400

    fix

--- slurm/ctp_stack_upsampling2d.sh
+++ slurm/ctp_stack_upsampling2d.sh
@@ -24,7 +24,7 @@ $HOME/costar_plan/costar_models/scripts/ctp_model_tool \
 	--model predictor \
 	--data_file $HOME/work/ctp_test2.npz \
 	--lr 0.001 \
-	 --model_directory $HOME/.costar/models_stack2/ \
+	 --model_directory $HOME/.costar/models_stack_us2d/ \
 	--optimizer adam \
   --upsampling upsampling \
 	--batch_size 64
