29615f8f53a4c2b3d511d29b148ee71496c07138
Chris Paxton <cpaxton3@jhu.edu>
2017-10-02 19:53:01 -0400

    updating scripts to start many

--- slurm/start_ctp_stacks.sh
+++ slurm/start_ctp_stacks.sh
@@ -6,22 +6,15 @@ set -u
 
 module load tensorflow/cuda-8.0/r1.3 
 
-#for $i in {1..10}
-#do
-#	# start the script
-#	$HOME/costar_plan/costar_models/scripts/ctp_model_tool \
-#		--features multi \
-#		-e 100 \
-#		--model predictor \
-#		--data_file $HOME/work/ctp_test2.npz \
-#		--lr 0.001 \
-#		 --model_directory $HOME/.costar/models_stack/ \
-#		--optimizer adam \
-#		--batch_size 64
-#done
+for $lr in [0.1,0.01,0.001]
+do
+	for $dr in [0.1, 0.2, 0.5]
+	do
+		sbatch ctp.sh $lr $dr
+done
 
 # ======================================
-sbatch ctp_stack.sh
-sbatch ctp_stack_upsampling2d.sh
-sbatch ctp_stack2.sh
-sbatch ctp_stack_small.sh
+#sbatch ctp_stack.sh
+#sbatch ctp_stack_upsampling2d.sh
+#sbatch ctp_stack2.sh
+#sbatch ctp_stack_small.sh
