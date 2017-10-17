d4642fd00ff6f4ac6232d5d0fccd2e020795767e
Chris Paxton <cpaxton3@jhu.edu>
2017-10-17 14:46:46 -0700

    undo

--- costar_models/python/costar_models/multi_sampler.py
+++ costar_models/python/costar_models/multi_sampler.py
@@ -359,8 +359,8 @@ class RobotMultiPredictionSampler(RobotMultiHierarchical):
 
         # Preprocess values
         value_target = np.array(value > 1.,dtype=float)
-        q[:,3:] = q[:,3:] / np.pi
-        q_target[:,3:] = q_target[:,3:] / np.pi
+        #q[:,3:] = q[:,3:] / np.pi
+        #q_target[:,3:] = q_target[:,3:] / np.pi
         qa /= np.pi
 
         o_target = np.squeeze(self.toOneHot2D(o_target, self.num_options))
