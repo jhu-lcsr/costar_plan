9491b0f771de3b23f008cae37187aa5f3ac67f1c
Andrew Hundt <ATHundt@gmail.com>
2017-10-24 00:19:13 -0400

    grasp_dataset.py typo fix

--- costar_google_brainrobotdata/grasp_dataset.py
+++ costar_google_brainrobotdata/grasp_dataset.py
@@ -640,8 +640,8 @@ class GraspDataset(object):
                     [fixed_feature_op_dict[base_to_endeffector_transform_name],
                      fixed_feature_op_dict[base_T_endeffector_final_close_gripper_name],
                      'vec_quat_7'],
-                    name='py_func/current_endeffector_to_final_endeffector_feature/vec_quat_7',
-                    tf.float32)
+                    tf.float32,
+                    name='py_func/current_endeffector_to_final_endeffector_feature/vec_quat_7')
                 current_to_end_op.set_shape([7])
                 current_to_end_name = 'move_to_grasp/{:03}/reached_pose/transforms/endeffector_current_T_endeffector_final/vec_quat_7'.format(j)
                 fixed_feature_op_dict[current_to_end_name] = current_to_end_op
@@ -658,8 +658,8 @@ class GraspDataset(object):
                     [fixed_feature_op_dict[base_to_endeffector_transform_name],
                      fixed_feature_op_dict[base_T_endeffector_final_close_gripper_name],
                      'vec_sin_cos_5'],
-                    name='py_func/current_endeffector_to_final_endeffector_feature/vec_sin_cos_5',
-                    tf.float32)
+                    tf.float32,
+                    name='py_func/current_endeffector_to_final_endeffector_feature/vec_sin_cos_5')
                 current_to_end_op.set_shape([5])
                 current_to_end_name = 'move_to_grasp/{:03}/reached_pose/transforms/endeffector_current_T_endeffector_final/vec_sin_cos_5'.format(j)
                 fixed_feature_op_dict[current_to_end_name] = current_to_end_op
@@ -737,7 +737,7 @@ class GraspDataset(object):
                 grasp_geometry.grasp_dataset_to_surface_relative_transform,
                 # parameters for call to surface_relative_transform function call
                 [depth_clear_view, camera_intrinsics_matrix, camera_T_base, fixed_feature_op_dict[final_pose_op]],
-                name='py_func/grasp_dataset_to_surface_relative_transform', tf.float32)
+                tf.float32, name='py_func/grasp_dataset_to_surface_relative_transform')
             # vector quaternion array of size 7
             current_to_end_op.set_shape([7])
             fixed_feature_op_dict[current_to_end_name] = current_to_end_op
