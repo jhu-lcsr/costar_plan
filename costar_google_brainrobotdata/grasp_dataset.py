4d3fc2e11bd8f70b7f3eb71e826997e6e62f368e
Andrew Hundt <ATHundt@gmail.com>
2017-10-24 20:21:48 -0400

    grasp_dataset.py py_func calls set stateful=False https://www.tensorflow.org/api_docs/python/tf/py_func

--- costar_google_brainrobotdata/grasp_dataset.py
+++ costar_google_brainrobotdata/grasp_dataset.py
@@ -641,6 +641,7 @@ class GraspDataset(object):
                      fixed_feature_op_dict[base_T_endeffector_final_close_gripper_name],
                      'vec_quat_7'],
                     tf.float32,
+                    stateful=False,
                     name='py_func/current_endeffector_to_final_endeffector_feature/vec_quat_7')
                 current_to_end_op.set_shape([7])
                 current_to_end_name = 'move_to_grasp/{:03}/reached_pose/transforms/endeffector_current_T_endeffector_final/vec_quat_7'.format(j)
@@ -659,6 +660,7 @@ class GraspDataset(object):
                      fixed_feature_op_dict[base_T_endeffector_final_close_gripper_name],
                      'vec_sin_cos_5'],
                     tf.float32,
+                    stateful=False,
                     name='py_func/current_endeffector_to_final_endeffector_feature/vec_sin_cos_5')
                 current_to_end_op.set_shape([5])
                 current_to_end_name = 'move_to_grasp/{:03}/reached_pose/transforms/endeffector_current_T_endeffector_final/vec_sin_cos_5'.format(j)
@@ -737,7 +739,7 @@ class GraspDataset(object):
                 grasp_geometry.grasp_dataset_to_surface_relative_transform,
                 # parameters for call to surface_relative_transform function call
                 [depth_clear_view, camera_intrinsics_matrix, camera_T_base, fixed_feature_op_dict[final_pose_op]],
-                tf.float32, name='py_func/grasp_dataset_to_surface_relative_transform')
+                tf.float32, stateful=False, name='py_func/grasp_dataset_to_surface_relative_transform')
             # vector quaternion array of size 7
             current_to_end_op.set_shape([7])
             fixed_feature_op_dict[current_to_end_name] = current_to_end_op
