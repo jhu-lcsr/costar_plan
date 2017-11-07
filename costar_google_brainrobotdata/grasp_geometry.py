3b0f5903d34a29a7e2f2ebecd6b8de74afd3f6f4
Andrew Hundt <ATHundt@gmail.com>
2017-11-06 22:34:19 -0500

    grasp_geometry.py fix link in comment

--- costar_google_brainrobotdata/grasp_geometry.py
+++ costar_google_brainrobotdata/grasp_geometry.py
@@ -22,7 +22,7 @@ except ImportError:
     print('eigen and sva not available, skipping components utilizing 3D geometry algorithms.'
           'To install run the script at'
           'https://github.com/ahundt/robotics_setup/blob/master/robotics_tasks.sh'
-          'or follow the instructions at https://github.com/jrl-umi3218/SpaceVecAlg'
+          'or follow the instructions at https://github.com/jrl-umi3218/Eigen3ToPython'
           'and https://github.com/jrl-umi3218/SpaceVecAlg and make sure python bindings'
           'are enabled.')
 
