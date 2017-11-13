a4c54c076de6c4f3484267d526a18bcf78403f85
Andrew Hundt <ATHundt@gmail.com>
2017-11-13 14:55:32 -0500

    random_crop_parameters.py whitespace and code layout fixes, random crop timing fixes

--- costar_google_brainrobotdata/grasp_dataset.py
+++ costar_google_brainrobotdata/grasp_dataset.py
@@ -925,21 +925,21 @@ class GraspDataset(object):
         """Get random offset for crop.
         """
         offset = rcp.random_crop_parameters(tf.constant([FLAGS.sensor_image_height, FLAGS.sensor_image_width, 3],
-                                                    name='sensor_image_height_width'),
-                                            tf.constant([FLAGS.random_crop_height, FLAGS.random_crop_width,3],
-                                                    name='random_image_height_width'))
+                                                        name='sensor_image_height_width'),
+                                            tf.constant([FLAGS.random_crop_height, FLAGS.random_crop_width, 3],
+                                                        name='random_image_height_width'))
         return offset
 
-    def _depth_preprocessing(self, depth_image_op, offset, intrinsics_matrix_op,random_crop=FLAGS.random_crop):
+    def _depth_preprocessing(self, depth_image_op, offset, intrinsics_matrix_op, random_crop=FLAGS.random_crop):
         """Randomly crop depth image, return cropped image and intrinsics.
         """
-        with tf.name_scope('depth_preprocessing'):                           
-            depth_image_op = tf.squeeze(depth_image_op,0)
-            if random_crop:                                  
-                output_image_list = rcp.crop_images(image_list=depth_image_op, 
-                                                    size=tf.constant([FLAGS.random_crop_height, FLAGS.random_crop_width,1]), 
+        with tf.name_scope('depth_preprocessing'):
+            depth_image_op = tf.squeeze(depth_image_op, 0)
+            if random_crop:
+                output_image_list = rcp.crop_images(image_list=depth_image_op,
+                                                    size=tf.constant([FLAGS.random_crop_height, FLAGS.random_crop_width, 1]),
                                                     offset=offset)
-                cropped_intrinsics_matrix_op =rcp.crop_image_intrinsics(intrinsics_matrix_op,offset)
+                cropped_intrinsics_matrix_op = rcp.crop_image_intrinsics(intrinsics_matrix_op, offset)
         return tf.cast(output_image_list, tf.float32), cropped_intrinsics_matrix_op
 
     def _rgb_preprocessing(self, rgb_image_op, offset,
@@ -955,9 +955,9 @@ class GraspDataset(object):
             # make sure the shape is correct
             rgb_image_op = tf.squeeze(rgb_image_op)
             # apply image augmentation and imagenet preprocessing steps adapted from keras
-            if random_crop:                                    
-                rgb_image_op = rcp.crop_images(image_list=rgb_image_op, 
-                                               size=tf.constant([FLAGS.random_crop_height, FLAGS.random_crop_width, 3]), 
+            if random_crop:
+                rgb_image_op = rcp.crop_images(image_list=rgb_image_op,
+                                               size=tf.constant([FLAGS.random_crop_height, FLAGS.random_crop_width, 3]),
                                                offset=offset)
             if resize:
                 rgb_image_op = tf.image.resize_images(rgb_image_op,
@@ -1033,7 +1033,7 @@ class GraspDataset(object):
             step='view_clear_scene'
         )
 
-        
+
         # get the feature names for the sequence of rgb images
         # in which movement towards the close gripper step is made
         rgb_move_to_grasp_steps = self.get_time_ordered_features(
@@ -1087,6 +1087,12 @@ class GraspDataset(object):
             feature_type='grasp_success'
         )
 
+        # get random crop offset parameters so that cropping will be done consistently.
+        sensor_dim_tensor = tf.constant([FLAGS.sensor_image_height, FLAGS.sensor_image_width, 3], name='sensor_image_height_width')
+        crop_dim_tensor = tf.constant([FLAGS.random_crop_height, FLAGS.random_crop_width, 3], name='random_image_height_width')
+        # TODO(ahundt) should offset be calculated here or in the loop below? This affects how much variation there is in cropping per batch.
+        offset = rcp.random_crop_parameters(sensor_dim_tensor, crop_dim_tensor)
+
         # our training batch size will be batch_size * grasp_steps
         # because we will train all grasp step images w.r.t. final
         # grasp success result value
@@ -1095,6 +1101,7 @@ class GraspDataset(object):
         # simplified_network_grasp_command_op
         simplified_grasp_command_op_batch = []
         grasp_success_op_batch = []
+
         # go through every element in the batch
         for fixed_feature_op_dict, sequence_feature_op_dict in feature_op_dicts:
             # print('fixed_feature_op_dict: ', fixed_feature_op_dict)
@@ -1102,7 +1109,7 @@ class GraspDataset(object):
             # TODO(ahundt) move squeeze steps into dataset api if possible
             camera_intrinsics_matrix = fixed_feature_op_dict['camera/intrinsics/matrix33']
             pregrasp_image_rgb_op = fixed_feature_op_dict[rgb_clear_view[0]]
-            offset = self._get_offset()
+
             pregrasp_image_rgb_op = self._rgb_preprocessing(pregrasp_image_rgb_op, offset,
                                                             imagenet_mean_subtraction=imagenet_mean_subtraction,
                                                             random_crop=random_crop,
@@ -1121,7 +1128,7 @@ class GraspDataset(object):
                         raise ValueError('ERROR: the time step of the grasp step does not match the motion command params, '
                                          'make sure the lists are indexed correctly!')
                     pregrasp_op_batch.append(pregrasp_image_rgb_op)
-                    grasp_step_rgb_feature_op = self._rgb_preprocessing(fixed_feature_op_dict[grasp_step_rgb_feature_name],offset)
+                    grasp_step_rgb_feature_op = self._rgb_preprocessing(fixed_feature_op_dict[grasp_step_rgb_feature_name], offset)
                     grasp_step_op_batch.append(grasp_step_rgb_feature_op)
                     # print("fixed_feature_op_dict[pose_op_param]: ", fixed_feature_op_dict[pose_op_param])
                     simplified_grasp_command_op_batch.append(fixed_feature_op_dict[pose_op_param])
--- costar_google_brainrobotdata/random_crop_parameters.py
+++ costar_google_brainrobotdata/random_crop_parameters.py
@@ -8,8 +8,6 @@ from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import gen_random_ops
 from tensorflow.python.ops import math_ops
 
-from tensorflow.python.ops.gen_random_ops import *
-
 
 def random_crop_parameters(input_shape, output_shape, seed=None, name=None):
     """ Generate crop parameter by random.
@@ -19,7 +17,7 @@ def random_crop_parameters(input_shape, output_shape, seed=None, name=None):
         seed: Python integer. Used to create a random seed. See tf.set_random_seed for behavior.
         name: A name for this operation.
     """
-    with ops.name_scope(name, "random_crop_parameters", [input_shape, output_shape]) as name:
+    with tf.name_scope(name, "random_crop_parameters", [input_shape, output_shape]) as name:
         input_shape = ops.convert_to_tensor(input_shape,  dtype=dtypes.int32, name="input_shape")
         output_shape = ops.convert_to_tensor(output_shape, dtype=dtypes.int32, name="output_shape")
         check = control_flow_ops.Assert(
@@ -36,6 +34,7 @@ def random_crop_parameters(input_shape, output_shape, seed=None, name=None):
 
     return offset
 
+
 def crop_images(image_list, size, offset, name=None):
     """ Crop color image and depth image by random.
         # Params
@@ -45,35 +44,35 @@ def crop_images(image_list, size, offset, name=None):
         seed: Python integer. Used to create a random seed. See tf.set_random_seed for behavior.
         name: A name for this operation.
     """
-    if isinstance(image_list, list):
-        cropped_image_list = []
-        with ops.name_scope(name, "crop_images", [image_list, size]) as name:
+    with tf.name_scope(name, "crop_images", [image_list, size]) as name:
+        if isinstance(image_list, list):
+            cropped_image_list = []
             size = ops.convert_to_tensor(size, dtype=dtypes.int32, name="size")
             for image in image_list:
                 image = ops.convert_to_tensor(image, name="image")
                 image = array_ops.slice(image, offset, size, name=name)
-                cropped_image_list.append(image) 
-    else:
-        with ops.name_scope(name, "crop_images", [image_list, size]) as name:
+                cropped_image_list.append(image)
+        else:
             size = ops.convert_to_tensor(size, dtype=dtypes.int32, name="size")
             image_list = ops.convert_to_tensor(image_list, name="image")
             cropped_image_list = array_ops.slice(image_list, offset, size, name=name)
-            
-    return cropped_image_list 
 
-def crop_image_intrinsics(camera_intrinsics_matrix,offset,name=None):
+    return cropped_image_list
+
+
+def crop_image_intrinsics(camera_intrinsics_matrix, offset, name=None):
     """ Calculate the intrinsic after crop.
         # Params
         camera_intrinsics_matrix: intrinsic matrix before cropping.
         offset: offset used in cropping.
     """
     #offset should be array can be access by index
-    with ops.name_scope(name, "crop_image_intrinsics", [camera_intrinsics_matrix, offset]) as name:
-        offset_x = tf.gather(offset,tf.constant([0]))
-        offset_x = tf.pad(offset_x,tf.constant([[2,0,],[0,2]]))
-        offset_y = tf.gather(offset,tf.constant([1]))
-        offset_y = tf.pad(offset_x,tf.constant([[2,0,],[1,1]]))
+    with tf.name_scope(name, "crop_image_intrinsics", [camera_intrinsics_matrix, offset]) as name:
+        offset_x = tf.gather(offset, tf.constant([0]))
+        offset_x = tf.pad(offset_x, tf.constant([[2, 0], [0, 2]]))
+        offset_y = tf.gather(offset, tf.constant([1]))
+        offset_y = tf.pad(offset_x, tf.constant([[2, 0], [1, 1]]))
         camera_intrinsics_matrix = camera_intrinsics_matrix - offset_x - offset_y
-        
+
     return camera_intrinsics_matrix
 
