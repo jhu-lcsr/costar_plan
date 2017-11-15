5c023562bc9ecc1cdc079721ed977f8dff1e00a5
yding37 <yding37@jhu.edu>
2017-11-14 21:28:05 -0500

    make this file comply to pep8

--- costar_google_brainrobotdata/random_crop_parameters.py
+++ costar_google_brainrobotdata/random_crop_parameters.py
@@ -14,12 +14,16 @@ def random_crop_parameters(input_shape, output_shape, seed=None, name=None):
         # Params
         input_shape: shape of input image need to crop.
         output_shape: shape of image after cropping.
-        seed: Python integer. Used to create a random seed. See tf.set_random_seed for behavior.
+        seed: Python integer. Used to create a random seed.
+        See tf.set_random_seed for behavior.
         name: A name for this operation.
     """
-    with tf.name_scope(name, "random_crop_parameters", [input_shape, output_shape]) as name:
-        input_shape = ops.convert_to_tensor(input_shape,  dtype=dtypes.int32, name="input_shape")
-        output_shape = ops.convert_to_tensor(output_shape, dtype=dtypes.int32, name="output_shape")
+    with tf.name_scope(name, "random_crop_parameters",
+                       [input_shape, output_shape]) as name:
+        input_shape = ops.convert_to_tensor(
+            input_shape, dtype=dtypes.int32, name="input_shape")
+        output_shape = ops.convert_to_tensor(
+            output_shape, dtype=dtypes.int32, name="output_shape")
         check = control_flow_ops.Assert(
             math_ops.reduce_all(input_shape >= output_shape),
             ["Need input_shape >= output_shape ", input_shape, output_shape],
@@ -41,7 +45,8 @@ def crop_images(image_list, size, offset, name=None):
         image_list: input images need to crop.
         image_intrinsics: 3*3 matirx with focal lengthes, and principle points,
         size: output size of image after cropping.
-        seed: Python integer. Used to create a random seed. See tf.set_random_seed for behavior.
+        seed: Python integer. Used to create a random seed.
+        See tf.set_random_seed for behavior.
         name: A name for this operation.
     """
     with tf.name_scope(name, "crop_images", [image_list, size]) as name:
@@ -55,7 +60,8 @@ def crop_images(image_list, size, offset, name=None):
         else:
             size = ops.convert_to_tensor(size, dtype=dtypes.int32, name="size")
             image_list = ops.convert_to_tensor(image_list, name="image")
-            cropped_image_list = array_ops.slice(image_list, offset, size, name=name)
+            cropped_image_list = array_ops.slice(
+                image_list, offset, size, name=name)
 
     return cropped_image_list
 
@@ -66,13 +72,14 @@ def crop_image_intrinsics(camera_intrinsics_matrix, offset, name=None):
         camera_intrinsics_matrix: intrinsic matrix before cropping.
         offset: offset used in cropping.
     """
-    #offset should be array can be access by index
-    with tf.name_scope(name, "crop_image_intrinsics", [camera_intrinsics_matrix, offset]) as name:
+    # offset should be array can be access by index
+    with tf.name_scope(name, "crop_image_intrinsics",
+                       [camera_intrinsics_matrix, offset]) as name:
         offset_x = tf.gather(offset, tf.constant([0]))
         offset_x = tf.pad(offset_x, tf.constant([[2, 0], [0, 2]]))
         offset_y = tf.gather(offset, tf.constant([1]))
         offset_y = tf.pad(offset_x, tf.constant([[2, 0], [1, 1]]))
-        camera_intrinsics_matrix = camera_intrinsics_matrix - offset_x - offset_y
+        camera_intrinsics_matrix = camera_intrinsics_matrix - offset_x
+        - offset_y
 
     return camera_intrinsics_matrix
-
