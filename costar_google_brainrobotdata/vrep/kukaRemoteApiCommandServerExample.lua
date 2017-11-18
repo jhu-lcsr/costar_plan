76260808156de2c511dde43f42f1e3eb0ef00866
Andrew Hundt <ATHundt@gmail.com>
2017-11-18 17:18:10 -0500

    vrep_grasp.py rgb and depth images now display data, though the contents don't look correct yet

--- costar_google_brainrobotdata/vrep/kukaRemoteApiCommandServerExample.lua
+++ costar_google_brainrobotdata/vrep/kukaRemoteApiCommandServerExample.lua
@@ -107,6 +107,8 @@ insertPointCloud_function=function(inInts,inFloats,inStrings,inBuffer)
     --
     -- inInts: parent_handle, poseEntries, cloudFloatCount, options
     -- inFloats: the point cloud points with quantity defined by cloudFloatCount
+    -- inStrings: [display_name, rgb_sensor_display_name]
+    --      the second parameter for the rgb sensor is optional.
     -- inBuffer: the pixel colors
     if #inStrings>=1 and #inInts>=6 then
         cloudHandle=-1
@@ -137,9 +139,72 @@ insertPointCloud_function=function(inInts,inFloats,inStrings,inBuffer)
         end
         -- Insert the points and color elements into the point cloud
         simInsertPointsIntoPointCloud(cloudHandle, options, inFloats, colors)
+
+        -- display the color image if a sensor object exists
+        if #inStrings>=2 then
+            rgb_camera_display_name = inStrings[2]
+            kcam_rgb_handle = simGetObjectHandle(rgb_camera_display_name)
+            simSetVisionSensorImage(kcam_rgb_handle, colors)
+        end
+
         return {cloudHandle},{},{},'' -- return the handle of the created dummy
     else
         simAddStatusbarMessage('insertPointCloud_function call failed because incorrect parameters were passed.')
+    end
+end
+
+
+setVisionSensorImage_function=function(inInts,inFloats,inStrings,inBuffer)
+    -- inserts points into a point cloud created via createPointCloud_function
+    -- Function called over python remote API, see create_point_cloud.
+    -- See simSetVisionSensorImage V-REP docs for packed parameter details,
+    -- and V-REP remote API for why they are packed.
+    --
+    -- Be sure to create the sensor in your simulation, and to correctly set
+    -- the image dimensions it should expect.
+    --
+    -- see http://www.coppeliarobotics.com/helpFiles/en/apiFunctions.htm#simSetVisionSensorImage
+    --
+    -- inInts: [parent_handle, num_floats, is_greyscale, color_size]
+    -- inFloats: the point cloud points with qepuantity defined by cloudFloatCount
+    -- inBuffer: the pixel colors
+    if #inStrings>=1 and #inInts>=4 then
+        sensorHandle=-1
+        -- Get the existing point cloud's handle or create a new one
+        if pcall(function()
+            sensorHandle=simGetObjectHandle(inStrings[1])
+        end) == false then
+            simAddStatusbarMessage('setVisionSensorImage_function could not find the following scene object: ' .. inStrings[1])
+            return
+        end
+
+        -- Set the pose
+        parent_handle=inInts[1]
+        -- commented because we will set position and orientation separately later
+        -- because point clouds simply have too much data to unpack
+        -- setObjectRelativeToParentWithPoseArray(cloudHandle, parent_handle, inFloats)
+        -- Get the number of float entries used for the pose
+        num_floats=inInts[2]
+        -- 0 if greyscale, 1 otherwise
+        is_greyscale=inInts[3]
+        -- number of entries in inBuffer
+        color_size=inInts[4]
+        -- get the colors from the buffer
+        colors = simUnpackUInt8Table(inBuffer)
+        if is_greyscale == 1 then
+            -- http://www.coppeliarobotics.com/helpFiles/en/apiFunctions.htm#simSetVisionSensorImage
+            -- indicates adding this handle flag to indicate greyscale
+            sensorHandle = sensorHandle + sim_handleflag_greyscale
+        end
+
+        if #inFloats > 0 then
+            simSetVisionSensorImage(sensorHandle, inFloats)
+        else
+            simSetVisionSensorImage(sensorHandle, colors)
+        end
+        return {sensorHandle},{},{},'' -- return the handle of the created dummy
+    else
+        simAddStatusbarMessage('setVisionSensorImage_function call failed because incorrect parameters were passed.')
     end
 end
 
