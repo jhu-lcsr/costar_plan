64fcd208052ffc03be75db5d4e90c1dcf7fa0842
Chris Paxton <cpaxton3@jhu.edu>
2017-11-21 14:09:02 -0800

    updating things

--- costar_models/python/costar_models/datasets/__init__.py
+++ costar_models/python/costar_models/datasets/__init__.py
@@ -1,2 +1,4 @@
 from .tfrecord import TFRecordConverter
 from .npz import NpzDataset
+from .h5f import H5fDataset
+from .npy_generator import NpzGeneratorDataset
---
+++ costar_models/python/costar_models/datasets/h5f_generator.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+
+import numpy as np
+import os
+
+class H5fGeneratorDataset(object):
+    '''
+    Placeholder class for use with the ctp training tool. This one generates
+    samples from an h5f encoded set of data.
+    '''
+    pass
+ 
