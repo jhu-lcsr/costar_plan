d72f28788419bbac468d1a1231c4c622575a80b7
Chris Paxton <cpaxton3@jhu.edu>
2017-11-27 20:59:07 -0500

    clean up

--- costar_task_plan/python/costar_task_plan/robotics/workshop/__init__.py
+++ costar_task_plan/python/costar_task_plan/robotics/workshop/__init__.py
@@ -1,18 +1,6 @@
 
 from config import UR5_C_MODEL_CONFIG
 
-<<<<<<< HEAD
-from parse import GetWorkshopParser
-from parse import ParseWorkshopArgs
-
-
-__all__ = [
-    # Parse arguments
-    "ParseWorkshopArgs", "GetWorkshopParser",
-    # Configuration files
-    "UR5_C_MODEL_CONFIG"
-    ]
-=======
 from listeners import ListenerManager
 from parse import GetWorkspaceParser, ParseWorkspaceArgs
 
@@ -25,5 +13,4 @@ __all__ = [
 
     # Parsing utils
     "GetWorkspaceParser", "ParseWorkspaceArgs",
-        ]
->>>>>>> a8c68be115af8788d19bcd7aec6c74eee5f06225
+]
