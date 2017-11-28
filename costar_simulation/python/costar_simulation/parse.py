c44d56d74b21a656987454031b5cbe1bf7eac107
Chris Paxton <cpaxton3@jhu.edu>
2017-11-28 12:41:10 -0800

    add parsing code for gazebo sim

---
+++ costar_simulation/python/costar_simulation/parse.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+
+import argparse
+import sys
+
+from costar_task_plan.simulation import GetSimulationParser
+
+def GetLaunchOptions():
+    return ["ur5"]
+
+def GetExperimentOptions():
+    return ["magnetic_assembly",
+            "stack",
+            "tables"]
+
+def _assemblyCases():
+    cases = ["double","training","finished_assembly"]
+    for i in range(1,11):
+        cases.append("double%d"%i)
+    for i in range(1,8):
+        cases.append("training%d"%i)
+    for i in range(1,3):
+        cases.append("assembly%d"%i)
+    return cases
+
+def ParseGazeboArgs():
+    parser = GetSimulationParser()
+    parser.add_argument('--launch',
+                        help="ROS launch file to start Gazebo simulation",
+                        default="ur5",
+                        choices=GetLaunchOptions())
+    parser.add_argument("--experiment",
+                        help="Experiment file that configures task",
+                        default="magnetic_assembly",
+                        choices=GetExperimentOptions())
+    parser.add_argument("--case",
+                        help="Case for magnetic assembly experiment",
+                        default="double1",
+                        choices=_assemblyCases())
+    return vars(parser.parse_args())
+
+
