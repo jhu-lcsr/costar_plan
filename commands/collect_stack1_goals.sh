97efe5ecb098b059e5a506cb50222edd2ad3727c
Chris Paxton <cpaxton3@jhu.edu>
2017-12-04 06:27:17 -0800

    make some updates

--- commands/collect_stack1_goals.sh
+++ commands/collect_stack1_goals.sh
@@ -6,7 +6,7 @@ echo "Starting data collection, must be either 'start' or 'gui_start'"
 rosrun costar_bullet $1 \
   --robot ur5 --task stack1 --agent task \
   -i 10000 --features multi  --verbose \
-  --seed 3158 \
+  --seed 5157 \
   --cpu \
   --save --data_file test2.h5f \
   --collection_mode goal
