36e9fb81f56aae170bd76e97a8383b07fd793953
Chris Paxton <cpaxton3@jhu.edu>
2017-12-05 12:43:56 +0100

    updates

---
+++ ctp_tom/python/ctp_tom/__init__.py
@@ -0,0 +1,10 @@
+
+from .parse import *
+from .manager import *
+from .experiment import *
+
+__all__ = ["TomManager",
+        "TomExperiment",
+        "GetExperiments",
+        "MakeExperiment",
+        "FakeOrangesExperiment",]
