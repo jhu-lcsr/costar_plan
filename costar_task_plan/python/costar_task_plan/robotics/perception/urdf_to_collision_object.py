7e72725927e6638221fa758ef271c3ac14dd6333
Chris Paxton <cpaxton3@jhu.edu>
2017-12-07 11:49:34 +0100

    update urdf collision object thing

--- costar_task_plan/python/costar_task_plan/robotics/perception/urdf_to_collision_object.py
+++ costar_task_plan/python/costar_task_plan/robotics/perception/urdf_to_collision_object.py
@@ -13,52 +13,89 @@ from std_srvs.srv import EmptyResponse
 import pyassimp
 import rospy
 
-def GetUrdfCollisionObject(name, rosparam=None, urdf=None):
+def GetUrdfCollisionObject(name, rosparam):
     '''
     This function loads a collision object specified as an URDF for integration
     with perception and MoveIt.
     '''
     if rospy.is_shutdown():
         raise RuntimeError('URDF must be loaded from the parameter server.')
-    elif rosparam is None and urdf is None:
+    elif rosparam is None:
         raise RuntimeError('no model found!')
 
-    if urdf is None:
-        urdf = URDF.from_parameter_server(rosparam)
-        add = True
-    else:
-        add = False
+    urdf = URDF.from_parameter_server(rosparam)
 
+    return urdf
+
+def _getCollisionObject(name, urdf, pose, operation):
+    '''
+    This function takes an urdf and a TF pose and updates the collision
+    geometry associated with the current planning scene.
+    '''
     co = CollisionObject()
     co.id = name
-    if add:
-        co.operation = CollisionObject.ADD
-    else:
-        co.operation = CollisionObject.MOVE
+    co.operation = operation
+
+    if len(urdf.links) > 1:
+        rospy.logwarn('collison object parser does not currently support kinematic chains')
 
     for l in urdf.links:
-        if co.operation == CollisionObject.ADD:
-            for c in l.collisions:
-                # check type
+        # Compute the link pose based on the origin
+        if l.origin is None:
+            link_pose = pose
+        else:
+            link_pose = pose * kdl.Frame(
+                    kdl.Rotation.RPY(*l.origin.rpy),
+                    kdl.Vector(*l.origin.xyz))
+        for c in l.collisions:
+            # check type
+            if co.operation == CollisionObject.ADD:
+                # Only update the geometry if we actually need to add the
+                # object to the collision scene.
                 if isinstance(c, urdf_parser_py.urdf.Box):
                     size = c.size
                     element = SolidPrimitive
                     element.type = SolidPrimitive.BOX
                     element.dimensions = list(c.geometry.size)
                     co.primitives.append(element)
-                    co.primitive_poses.append(pose)
+                elif isinstance(c, urdf_parser_py.urdf.Mesh):
+                    scale = (1,1,1)
+                    if c.scale is not None:
+                        scale = c.scale
+                    element = _loadMesh(c, scale)
+                    co.meshes.append(element)
 
-    return co, urdf
+            pose = kdl.Frame(
+                    kdl.Rotation(*c.origin.rpy),
+                    kdl.Vector(*c.origin.xyz))
+            pose = link_pose * pose
+            if primitive:
+                co.primitive_poses.append(pose)
+            else:
+                # was a mesh
+                co.mesh_poses.append(pose)
+
+    return co
 
 class CollisionObjectManager(object):
-    def __init__(self):
+    '''
+    Creates and aggregates information from multiple URDF objects and publishes
+    to a planning scene based on TF positions of these objects.
+    '''
+
+    def __init__(self, root="/world", tf_listener=None):
         self.objs = {}
         self.urdfs = {}
         self.frames = {}
+        if tf_listener is None:
+            self.listener = tf.TransformListener()
+        else:
+            self.listener = tf_listener
+        self.root = root
 
     def addUrdf(self, name, rosparam, tf_frame=None):
-        co, urdf = GetUrdfCollisionObject(name, rosparam, None)
-        self.objs[name] = co
+        urdf = _getUrdf(name, rosparam)
+        self.objs[name] = None
         self.urdfs[name] = urdf
         if tf_frame is None:
             tf_frame = name
@@ -66,6 +103,10 @@ class CollisionObjectManager(object):
 
     def tick(self):
         for name, urdf in self.urdfs.items():
-            co, _ = GetUrdfCollisionObject(name, rosparam=None, urdf=urdf)
+            if self.objs[name] == None:
+                operation = CollisionObject.ADD
+            else:
+                operation = CollisionObject.MOVE
+            co = _getCollisionObject(name, urdf, pose, operation)
 
 
