01cab4bb415783091f2d5145f5ffdec0a02d9619
Chris Paxton <cpaxton3@jhu.edu>
2017-12-07 11:54:13 +0100

    updating collision manager with mesh and other fixes

--- costar_task_plan/python/costar_task_plan/robotics/perception/urdf_to_collision_object.py
+++ costar_task_plan/python/costar_task_plan/robotics/perception/urdf_to_collision_object.py
@@ -27,6 +27,46 @@ def GetUrdfCollisionObject(name, rosparam):
 
     return urdf
 
+def _make_mesh(self, c, scale = (1, 1, 1)):
+    '''
+    This was taken from moveit commander and slightly modified.
+    '''
+    filename = c.geometry.filename
+    if pyassimp is False:
+        raise RuntimeError("Pyassimp needs patch https://launchpadlibrarian.net/319496602/patchPyassim.txt")
+    scene = pyassimp.load(filename)
+    if not scene.meshes or len(scene.meshes) == 0:
+        raise MoveItCommanderException("There are no meshes in the file")
+    if len(scene.meshes[0].faces) == 0:
+        raise MoveItCommanderException("There are no faces in the mesh")
+    
+    mesh = Mesh()
+    first_face = scene.meshes[0].faces[0]
+    if hasattr(first_face, '__len__'):
+        for face in scene.meshes[0].faces:
+            if len(face) == 3:
+                triangle = MeshTriangle()
+                triangle.vertex_indices = [face[0], face[1], face[2]]
+                mesh.triangles.append(triangle)
+    elif hasattr(first_face, 'indices'):
+        for face in scene.meshes[0].faces:
+            if len(face.indices) == 3:
+                triangle = MeshTriangle()
+                triangle.vertex_indices = [face.indices[0],
+                                           face.indices[1],
+                                           face.indices[2]]
+                mesh.triangles.append(triangle)
+    else:
+        raise RuntimeError("Unable to build triangles from mesh due to mesh object structure")
+    for vertex in scene.meshes[0].vertices:
+        point = Point()
+        point.x = vertex[0]*scale[0]
+        point.y = vertex[1]*scale[1]
+        point.z = vertex[2]*scale[2]
+        mesh.vertices.append(point)
+    pyassimp.release(scene)
+    return mesh
+
 def _getCollisionObject(name, urdf, pose, operation):
     '''
     This function takes an urdf and a TF pose and updates the collision
