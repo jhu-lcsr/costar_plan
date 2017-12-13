1c0e8553d02f99a8aa24bc294d4e02837968aac0
Chris Paxton <cpaxton3@jhu.edu>
2017-12-13 13:42:37 +0100

    update tag structure and add new messages

--- costar_task_plan/python/costar_task_plan/abstract/task_parser.py
+++ costar_task_plan/python/costar_task_plan/abstract/task_parser.py
@@ -60,6 +60,8 @@ class TaskParser(object):
     def __init__(self,
             action_naming_style=NAME_STYLE_UNIQUE,
             min_action_length=5, # ignore anything below this length
+            unknown_apply_before=4,
+            configs=[],
             *args, **kwargs):
         '''
         Create a task parser. This lets you load one demonstration in at a
@@ -75,6 +77,9 @@ class TaskParser(object):
         self.idle_tags = []
         self.unknown_tags = []
         self.resetDemonstration()
+        self.configs = configs
+        self.num_arms = len(self.configs)
+        self.unknown_apply_before = unknown_apply_before
 
     def addObjectClass(self, object_class):
         self.object_classes.add(object_class)
@@ -89,25 +94,27 @@ class TaskParser(object):
         self.classes_by_object[obj] = obj_class
 
     def _getActionName(self, action):
-        if action.base_name in self.ignore_actions:
+        if action.base_name in self.unknown_tags:
             return None
         elif self.action_naming_style == NAME_STYLE_SAME:
             return action.base_name
         elif self.action_naming_style == NAME_STYLE_UNIQUE:
+            if action.arm == ARM_LEFT:
+                arm = "left"
+            elif action.arm == ARM_RIGHT:
+                arm = "right"
             if action.object_acted_on is not None:
                 return "%s_%s"%(action.base_name, action.object_acted_on)
             else:
                 return "%s"%(action.base_name)
 
     def resetDemonstration(self):
-        self.prev = None
-        self.prev_t = None
-        self.action_start_t = None
+        self.data = []
 
     def addIdle(self, *args):
         self.idle_tags += list(args)
 
-    def addUnknwon(self, *args):
+    def addUnknown(self, *args):
         self.unknown_tags += list(args)
 
     def _addTransition(self, from_action, to_action):
@@ -134,7 +141,30 @@ class TaskParser(object):
         Call this to parse a single time step. This assumes you have properly
         called resetDemonstration() above.
         '''
+        self.data.append((t, objs, actions))
 
+    def processDemonstration(self):
+        prev_t = [None] * self.num_arms
+        prev = [None] * self.num_arms
+        action_start_t = [None] * self.num_arms
+        
+        # Preprocess to remove "Unknown" symbols
+        ACTION_IDX = 2
+        for i, (t, objs, actions) in enumerate(self.data):
+            for j, a in enumerate(actions):
+                if a.name in self.unknown_tags:
+                    reset_name = False
+                    for k in range(4):
+                        if i+k < len(self.data):
+                            next_name = self.data[i+k][ACTION_IDX][j].name
+                            if next_name not in self.unknown_tags:
+                                a.name = next_name
+                                reset_name = True
+                                break
+
+
+
+        # Loop over all time steps
         if self.prev_t is not None:
             # Trigger a sanity check to make sure we do not have any weird jumps in our file.
             dt = abs(self.prev_t - t)
