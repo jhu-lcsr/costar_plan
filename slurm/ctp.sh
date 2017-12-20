6fedb6edbd7cce71be48bc65f6e6cfe6e31363e7
Chris Paxton <cpaxton3@jhu.edu>
2017-12-20 11:13:06 -0500

    do not retrain image sampler

--- slurm/ctp.sh
+++ slurm/ctp.sh
@@ -19,23 +19,23 @@ module load tensorflow/cuda-8.0/r1.3
 export DATASET="ctp_dec"
 export MODELDIR="$HOME/.costar/models_stack_Z$1$3$2$4$5$6"
 
-$HOME/costar_plan/costar_models/scripts/ctp_model_tool \
-	--features multi \
-	-e 100 \
-	--model pretrain_image_encoder \
-	--data_file $HOME/work/$DATASET.h5f \
-	--lr $1 \
-	--dropout_rate $2 \
-	--decoder_dropout_rate $2 \
-  --model_directory $MODELDIR/ \
-	--optimizer $3 \
-  --use_noise true \
-  --steps_per_epoch 500 \
-  --noise_dim $5 \
-  --hypothesis_dropout $4 \
-  --upsampling conv_transpose \
-  --skip_connections $6 \
-  --batch_size 64
+#$HOME/costar_plan/costar_models/scripts/ctp_model_tool \
+#	--features multi \
+#	-e 100 \
+#	--model pretrain_image_encoder \
+#	--data_file $HOME/work/$DATASET.h5f \
+#	--lr $1 \
+#	--dropout_rate $2 \
+#	--decoder_dropout_rate $2 \
+#  --model_directory $MODELDIR/ \
+#	--optimizer $3 \
+#  --use_noise true \
+#  --steps_per_epoch 500 \
+#  --noise_dim $5 \
+#  --hypothesis_dropout $4 \
+#  --upsampling conv_transpose \
+#  --skip_connections $6 \
+#  --batch_size 64
 
 $HOME/costar_plan/costar_models/scripts/ctp_model_tool \
 	--features multi \
