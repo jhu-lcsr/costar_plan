57a7e92695f907992731373ffd46d1fb41566f03
Chris Paxton <cpaxton3@jhu.edu>
2017-12-22 17:18:58 -0500

    disable retrain image

--- slurm/ctp.sh
+++ slurm/ctp.sh
@@ -19,7 +19,7 @@ module load tensorflow/cuda-8.0/r1.3
 export DATASET="ctp_dec"
 export MODELDIR="$HOME/.costar/models_stack_Z2$1$3$2$4$5$6"
 
-if [ 2 -gt 1 ]
+if [ 0 -gt 1 ]
 then
   $HOME/costar_plan/costar_models/scripts/ctp_model_tool \
     --features multi \
