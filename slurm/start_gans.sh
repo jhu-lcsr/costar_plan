fe6eff8b7f0118a6f952a92307dd1d1fa64ced00
Yotam Barnoy <yotambarnoy@gmail.com>
2018-01-04 15:34:12 -0500

    slurm: minor change

--- slurm/start_gans.sh
+++ slurm/start_gans.sh
@@ -7,10 +7,10 @@ set -u
 module load tensorflow/cuda-8.0/r1.3 
 
 for lr in 0.001 0.0001; do
-  export GAN_METHOD=mae
-  export EPOCHS=100
-  echo "Starting LR=$lr, GAN_method=$GAN_METHOD, epochs=$epochs"
-  sbatch gan.sh $lr $GAN_METHOD $epochs
+  export gan_method=mae
+  export epochs=100
+  echo "Starting LR=$lr, GAN_method=$gan_method, epochs=$epochs"
+  sbatch gan.sh $lr $gan_method $epochs
 done
 
 
