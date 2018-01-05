2824a18b94e3372b21d38bd309451dbea3ddd1e3
Yotam Barnoy <yotambarnoy@gmail.com>
2018-01-05 12:24:56 -0500

    commands: learn_predictor minor tweak

--- commands/learn_predictor.sh
+++ commands/learn_predictor.sh
@@ -5,21 +5,21 @@
 #	--si 0 --lr 0.001 --optimizer nadam \
 #        --batch_size 64 
 
-export MODELDIR=$HOME/.costar/models
+export MODELDIR="$HOME/.costar/models"
 export LR=0.001
 export DR=0.2
 export OPT=adam
 export ND=0
 export HDR=0.2
+export DATASET=test2
 
-
-# image rep
 if true; then
+  echo "---- Image Representation ----"
   rosrun costar_models ctp_model_tool \
     --features multi \
     -e 100 \
     --model pretrain_image_encoder \
-    --data_file $DATASET\
+    --data_file ${DATASET}.h5f\
     --lr $LR \
     --dropout_rate $DR \
     --decoder_dropout_rate $DR \
@@ -33,13 +33,13 @@ if true; then
     --batch_size 64
 fi
 
-# extra encoding of data
-if true; then
+if false; then
+  echo "---- Image + Extra data representation ----"
   rosrun costar_models ctp_model_tool \
     --features multi \
     -e 100 \
     --model pretrain_sampler \
-    --data_file $HOME/work/$DATASET.h5f \
+    --data_file ${DATASET}.h5f \
     --lr $LR \
     --dropout_rate $DR \
     --decoder_dropout_rate $DR \
@@ -54,13 +54,13 @@ if true; then
 fi
 
 
-# transitions with all data
 if false; then
+  echo "---- Transitions + extra data ----"
   rosrun costar_models ctp_model_tool \
     --features multi \
     -e 100 \
     --model conditional_sampler2 \
-    --data_file $HOME/work/$DATASET.h5f \
+    --data_file ${DATASET}.h5f \
     --lr $LR \
     --dropout_rate $DR \
     --decoder_dropout_rate $DR \
@@ -75,11 +75,12 @@ if false; then
 fi
 
 # transitions, only images
+echo "---- Transitions with only images ----"
 rosrun costar_models ctp_model_tool \
   --features multi \
   -e 100 \
   --model conditional_image \
-  --data_file $HOME/work/$DATASET.h5f \
+  --data_file ${DATASET}.h5f \
   --lr $LR \
   --dropout_rate $DR \
   --decoder_dropout_rate $DR \
@@ -95,11 +96,12 @@ rosrun costar_models ctp_model_tool \
 
 # multiple parallel predictions
 if false; then
+  echo "---- Transitions with images + data ----"
   rosrun costar_models ctp_model_tool \
     --features multi \
     -e 100 \
     --model predictor2 \
-    --data_file $HOME/work/$DATASET.h5f \
+    --data_file ${DATASET}.h5f \
     --lr $LR \
     --dropout_rate $DR \
     --decoder_dropout_rate $DR \
