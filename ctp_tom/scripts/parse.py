1ffb048467b4a1f840cf341dd43a04d1da16b7b2
Chris Paxton <cpaxton3@jhu.edu>
2018-01-05 10:42:14 -0800

    adding more test objects to parsed scene

--- ctp_tom/scripts/parse.py
+++ ctp_tom/scripts/parse.py
@@ -69,6 +69,12 @@ def fakeTaskArgs():
     'endpoint': ['r_ee_link'],
     'high_table': ['tom_table'],
     'screw': ['screw_1', 'screw_2'],
+    'Cube_blue': ['blue1'],
+    'Cube_red': ['red1'],
+    'Cube_green': ['green1'],
+    'Cube_yellow': ['yellow1'],
+    'top_wood': ['top_wood'],
+    'bottom_wood': ['bottom_wood'],
   }
   return args
 
