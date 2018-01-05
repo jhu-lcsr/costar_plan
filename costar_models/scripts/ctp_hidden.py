fd98b9d1bbb0bdf386b3a10d8523eb12237a8697
Chris Paxton <cpaxton3@jhu.edu>
2018-01-05 09:55:11 -0800

    add script for hidden

---
+++ costar_models/scripts/ctp_hidden.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import matplotlib as mpl
+mpl.use("Agg")
+
+from costar_models import *
+from costar_models.datasets.npz import NpzDataset
+from costar_models.datasets.npy_generator import NpzGeneratorDataset
+from costar_models.datasets.h5f_generator import H5fGeneratorDataset
+
+'''
+Tool for running model training without the rest of the simulation/planning/ROS
+code. This should be more or less independent and only rely on a couple
+external features.
+'''
+
+def visualizeHiddenMain(args):
+    SetCPU(args)
+
+    data_file_info = args['data_file'].split('.')
+    data_type = data_file_info[-1]
+    root = ""
+    for i, tok in enumerate(data_file_info[:-1]):
+        if i < len(data_file_info)-1 and i > 0:
+            root += '.'
+        root += tok
+    if data_type == "npz":
+        dataset = NpzGeneratorDataset(root)
+        data = dataset.load(success_only = args['success_only'])
+    elif data_type == "h5f":
+        dataset = H5fGeneratorDataset(root)
+        data = dataset.load(success_only = args['success_only'])
+    else:
+        raise NotImplementedError('data type not implemented: %s'%data_type)
+
+    if 'model' in args and args['model'] is not None:
+        model = MakeModel(taskdef=None, **args)
+        if 'load_model' in args and args['load_model']:
+            model.load(world=None,**data)
+        train_generator = model.trainGenerator(dataset)
+        test_generator = model.testGenerator(dataset)
+
+   else:
+        raise RuntimeError('Must provide a model to load')
+
+if __name__ == '__main__':
+    args = ParseModelArgs()
+    if args['profile']:
+        import cProfile
+        cProfile.run('main(args)')
+    else:
+        visualizeHiddenMain(args)
