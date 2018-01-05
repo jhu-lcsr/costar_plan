2e105e56dc8f44372a09b5fb7dac31a14ac1aa2c
Chris Paxton <cpaxton3@jhu.edu>
2018-01-05 16:04:32 -0500

    adding some new things to create policies for robots

--- ctp_integration/python/ctp_integration/motion.py
+++ ctp_integration/python/ctp_integration/motion.py
@@ -2,14 +2,30 @@ from __future__ import print_function
 
 from costar_task_plan.abstract import AbstractOption
 
+from .arm_policies import CostarArmMotionPolicy
+
 class MotionOption(AbstractOption):
     '''
     This option calls out to the current segmenter and waits until termination.
     '''
 
-    def __init__(self, name="motion", goal):
+    def __init__(self, name="motion", world, goal, pose, pose_tolerance=(1e-4,1e-4)):
+        '''
+        Create a goal-directed motion.
+        '''
         super(MotionOption, self).__init__(name, True)
-        self.goal = goal # move to this particular object
+        self.goal = goal # move to this particular object if it exists
+        if goal is not None:
+            # Look up the specific ID of the object in the world
+            self.goal_id = world.getObjectId(goal)
+        else:
+            # Use negative numbers to specify missing goal frame
+            self.goal_id = -1
+        if pose is not None:
+            self.position, self.rotation = pose
+            self.position_tolerance, self.rotation_tolerance = pose_tolerance
+        else:
+            raise RuntimeError('Must specify pose.')
 
       def makeWorld(self, *args, **kwargs):
     '''
@@ -23,6 +39,7 @@ class MotionOption(AbstractOption):
       Get the gating condition for a specific option.
       - execution should continue until such time as this condition is true.
       '''
-      raise Exception('option.makePolicy not implemented!')
+      policy = CostarArmMotionPolicy(goal, pose)
+      condition = policy.finished
 
 
