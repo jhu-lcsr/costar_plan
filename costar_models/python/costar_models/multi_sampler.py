f69cd1b8df37274718af325f8073703be3661346
Chris Paxton <cpaxton3@jhu.edu>
2018-01-11 10:45:46 -0800

    add documentation and update

--- costar_models/python/costar_models/multi_sampler.py
+++ costar_models/python/costar_models/multi_sampler.py
@@ -332,13 +332,14 @@ class RobotMultiPredictionSampler(RobotMultiHierarchical):
         option = Input((48,),name="t_opt_in")
         x = AddConv2D(h, self.tform_filters, [1,1], 1, 0.)
         x0 = AddConv2D(h0, self.tform_filters, [1,1], 1, 0.)
-        y = AddDense(option, 64, "relu", 0., constraint=None, output=False)
-        x = Add()([x, x0])
+        x = Concatenate()([x, x0])
         x = AddConv2D(x, 64, [5,5], 1, 0.)
 
         # store this for skip connection
         skip = x
 
+        # Add dense information
+        y = AddDense(option, 64, "relu", 0., constraint=None, output=False)
         x = TileOnto(x, y, 64, (8,8))
         x = AddConv2D(x, self.tform_filters, [5,5], 1, 0.)
         x = Dropout(self.dropout_rate)(x)
@@ -351,11 +352,11 @@ class RobotMultiPredictionSampler(RobotMultiHierarchical):
                 constraint=None, output=False,)
         x = AddDense(x, 4*4*self.tform_filters, "relu", 0., constraint=None, output=False)
         x = Reshape([4,4,self.tform_filters])(x)
+        x = AddConv2DTranspose(x, self.tform_filters, [5,5], 2, 0.)
         # --- end ssm block
 
-        x = AddConv2DTranspose(x, self.tform_filters*2, [5,5], 2, 0.)
         if self.skip_connections or True:
-            x = Concatenate()([x, skip])
+            x = Concatenate()([x, x0])
         x = Dropout(self.dropout_rate)(x)
         #x = AddConv2DTranspose(x, self.tform_filters*2, [5,5], 2, 0.)
         #x = TileOnto(x0, x, self.tform_filters, (8,8))
