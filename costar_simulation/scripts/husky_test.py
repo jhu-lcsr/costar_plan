54f9d97b68d1ecbd29a2a150457721e7b119964b
Chris Paxton <cpaxton3@jhu.edu>
2018-01-16 12:32:38 -0800

    update husky data script

--- costar_simulation/scripts/husky_test.py
+++ costar_simulation/scripts/husky_test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 
 from gazebo_msgs.msg import ModelStates
--- costar_simulation/scripts/husky_test_data_write.py
+++ costar_simulation/scripts/husky_test_data_write.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image
