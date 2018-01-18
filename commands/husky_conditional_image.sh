cf9df482421658012fdbae1923aaf885c3e2e2be
Chris Paxton <cpaxton3@jhu.edu>
2018-01-18 13:21:04 -0800

    add some more options on to the end i guess

--- commands/husky_conditional_image.sh
+++ commands/husky_conditional_image.sh
@@ -3,4 +3,4 @@ rosrun costar_models ctp_model_tool \
   --model conditional_image \
   --data_file husky_data.npz \
   --lr 0.001 --dropout_rate 0.2 \
-  --features husky  --steps_per_epoch 500
+  --features husky  --steps_per_epoch 500 $1
