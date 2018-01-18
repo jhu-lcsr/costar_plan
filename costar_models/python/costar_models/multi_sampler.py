eda7b0a6154665a9eedaeff87cc425e4872356b1
Chris Paxton <cpaxton3@jhu.edu>
2018-01-18 10:02:25 -0800

    fix issues with creating custom-sized action sets

--- costar_models/python/costar_models/multi_sampler.py
+++ costar_models/python/costar_models/multi_sampler.py
@@ -311,7 +311,7 @@ class RobotMultiPredictionSampler(RobotMultiHierarchical):
         '''
         h = Input((8,8,self.encoder_channels),name="h_in")
         h0 = Input((8,8,self.encoder_channels),name="h0_in")
-        option = Input((48,),name="t_opt_in")
+        option = Input((self.num_options,),name="t_opt_in")
         x = AddConv2D(h, 64, [1,1], 1, 0.)
         x0 = AddConv2D(h0, 64, [1,1], 1, 0.)
 
