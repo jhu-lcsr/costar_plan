d34a004a75c4b8cd48c100e1eb922eff867a6881
Chris Paxton <cpaxton3@jhu.edu>
2018-01-18 11:54:39 -0800

    add the husky trainers

---
+++ slurm/start_ctp_husky.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+
+set -e
+set -x
+set -u
+
+module load tensorflow/cuda-8.0/r1.3 
+
+for lr in 0.01 0.001 0.0002 0.0001
+do
+  # just use the adam optimizer
+  for opt in adam
+  do
+    for loss in mae # logcosh
+    do
+    # what do we do about skip connections?
+    for skip in 0 # 1
+    do
+      # Noise: add extra ones with no noise at all
+      for noise_dim in 0 # 1 8 32
+      do
+        hd=true
+        for dr in 0. 0.1 0.2 # 0.3 0.4 0.5
+        do
+          echo "starting LR=$lr, Dropout=$dr, optimizer=$opt, noise=$noise_dim"
+          sbatch ctp_husky.sh $lr $dr $opt $noise_dim $loss
+        done
+      done
+    done
+    done
+  done
+done
+
