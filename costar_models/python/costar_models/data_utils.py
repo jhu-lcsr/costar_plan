46da37617f00b3ed92a9f89cec02eeb2e73cc4a9
Yotam Barnoy <yotambarnoy@gmail.com>
2018-01-18 16:07:57 -0500

    data_utils: undo GetNextGoal for now

--- costar_models/python/costar_models/data_utils.py
+++ costar_models/python/costar_models/data_utils.py
@@ -30,6 +30,7 @@ def MakeOption1h(option, num_labels):
     opt_1h[0,option] = 1.
     return opt_1h
 
+'''
 def GetNextGoal(imgs, labels):
     # Takes a list of target images and labels, and rotates them both
     # to the left by group
@@ -55,6 +56,39 @@ def GetNextGoal(imgs, labels):
         lrev[i] = write_l
         irev[i] = write_i
     return imgs, labels
+'''
+
+def GetNextGoal(I_target, o1):
+    img = np.zeros_like(I_target)
+    next_option = np.zeros_like(o1)
+    for i in range(o1.shape[0]):
+        #print('---')
+        cur = o1[i]
+        #print(i, o1, o1.shape, cur)
+        tmp = np.copy(o1)
+        tmp[:i] = cur
+        first_next = np.argmax(tmp != cur)
+        #print (tmp, tmp[first_next])
+        #print (cur, tmp[first_next])
+        if tmp[first_next] == cur:
+            # nothing else
+            first_next = -1
+        img[i] = I_target[first_next]
+        next_option[i] = tmp[first_next]
+        #print ('---')
+
+    debug_next_goals = False
+    if debug_next_goals:
+        import matplotlib.pyplot as plt
+        plt.subplot(3,1,1)
+        plt.imshow(I[i])
+        plt.subplot(3,1,2)
+        plt.imshow(I_target[i])
+        plt.subplot(3,1,3)
+        plt.imshow(img[i])
+        plt.show()
+
+    return img, next_option
 
 """
 # Data generator that creates sequences for input into PredNet.
