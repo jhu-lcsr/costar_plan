0e26112a481db747ee4088356eb8347afd8f6eef
Andrew Hundt <athundt@gmail.com>
2018-01-19 18:20:14 -0500

    grasp *.sh set data_dir to default download location

--- costar_google_brainrobotdata/pixelwise_prediction_grasp_train.sh
+++ costar_google_brainrobotdata/pixelwise_prediction_grasp_train.sh
@@ -1,1 +1,1 @@
-export CUDA_VISIBLE_DEVICES="0" && python2 grasp_train.py --batch_size=7 --epochs 300 --save_weights delta_depth_sin_cos_3 --grasp_model grasp_model_levine_2016_segmentation --optimizer SGD
+export CUDA_VISIBLE_DEVICES="0" && python2 grasp_train.py --batch_size=7 --epochs 300 --save_weights delta_depth_sin_cos_3_gaussian_loss --grasp_model grasp_model_levine_2016_segmentation --optimizer SGD --loss segmentation_gaussian_binary_crossentropy
