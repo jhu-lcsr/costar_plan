7acf3c647a9a458e98fc5b9d73250ba3a21c1167
Chris Paxton <cpaxton3@jhu.edu>
2018-01-19 11:36:57 -0800

    update scripts; add more

--- slurm/start_ctp_suturing.sh
+++ slurm/start_ctp_suturing.sh
@@ -20,10 +20,10 @@ do
       for noise_dim in 0 # 1 8 32
       do
         hd=true
-        for dr in 0.1 0.2 # 0. 0.3 0.4 0.5
+        for dr in 0. 0.1 0.2 # 0.3 0.4 0.5
         do
           echo "starting LR=$lr, Dropout=$dr, optimizer=$opt, noise=$noise_dim"
-          sbatch ctp.sh $lr $dr $opt $noise_dim $loss
+          sbatch ctp_suturing.sh $lr $dr $opt $noise_dim $loss
         done
       done
     done
