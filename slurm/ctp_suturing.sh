e6915ffd1c2b53de2e80f6dfaa7873c415605b3d
Chris Paxton <cpaxton3@jhu.edu>
2018-01-19 11:41:49 -0800

    cleaning up suturing scripts

--- slurm/ctp_suturing.sh
+++ slurm/ctp_suturing.sh
@@ -13,16 +13,14 @@ echo "Running $@ on $SLURMD_NODENAME ..."
 
 module load tensorflow/cuda-8.0/r1.3 
 
-export DATASET="husky_data"
+export DATASET="suturing_data2"
 export train_image_encoder=true
-export train_multi_encoder=false
-export train_predictor=false
 export learning_rate=$1
 export dropout=$2
 export optimizer=$3
 export noise_dim=$4
 export loss=$5
-export MODELDIR="$HOME/.costar/husky_$learning_rate$optimizer$dropout$noise_dim$loss"
+export MODELDIR="$HOME/.costar/suturing_$learning_rate$optimizer$dropout$noise_dim$loss"
 
 if $train_image_encoder
 then
@@ -34,27 +32,7 @@ then
     --data_file $HOME/work/$DATASET.npz \
     --lr $learning_rate \
     --dropout_rate $dropout \
-    --features husky \
-    --model_directory $MODELDIR/ \
-    --optimizer $optimizer \
-    --use_noise true \
-    --steps_per_epoch 500 \
-    --noise_dim $noise_dim \
-    --loss $loss \
-    --batch_size 64
-fi
-
-if $train_multi_encoder
-then
-  echo "Training encoder 2"
-  $HOME/costar_plan/costar_models/scripts/ctp_model_tool \
-    --features multi \
-    -e 100 \
-    --model pretrain_sampler \
-    --data_file $HOME/work/$DATASET.npz \
-    --lr $learning_rate \
-    --features husky \
-    --dropout_rate $dropout \
+    --features jigsaws \
     --model_directory $MODELDIR/ \
     --optimizer $optimizer \
     --use_noise true \
@@ -77,24 +55,4 @@ $HOME/costar_plan/costar_models/scripts/ctp_model_tool \
   --steps_per_epoch 500 \
   --loss $loss \
   --batch_size 64
-
-if $train_predictor
-then
-  $HOME/costar_plan/costar_models/scripts/ctp_model_tool \
-    --features multi \
-    -e 100 \
-    --model predictor \
-    --data_file $HOME/work/$DATASET.npz \
-    --features husky \
-    --lr $learning_rate \
-    --dropout_rate $dropout \
-    --model_directory $MODELDIR/ \
-    --optimizer $optimizer \
-    --use_noise true \
-    --steps_per_epoch 500 \
-    --loss $loss \
-    --skip_connections 1 \
-    --batch_size 64 # --retrain 
-    #--success_only \
-fi
 
