aed5e14597cf18eb7e38730e700998c76bd87939
Chris Paxton <cpaxton3@jhu.edu>
2018-01-20 15:44:57 -0800

    use the right data set

--- commands/jigsaws_pretrain_image_gan.sh
+++ commands/jigsaws_pretrain_image_gan.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 rosrun costar_models ctp_model_tool \
   --model pretrain_image_gan \
-  --data_file suturing_data.h5f \
+  --data_file suturing_data2.h5f \
   --lr 0.0002 \
   --dropout_rate 0.2 \
   --features jigsaws \
