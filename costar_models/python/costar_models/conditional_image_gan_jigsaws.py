d40a5895bdbe023f0ffc6b5110145a4a82f50a31
Yotam Barnoy <yotambarnoy@gmail.com>
2018-01-22 14:11:50 -0500

    Restore 12x16 hidden dimension

--- costar_models/python/costar_models/conditional_image_gan_jigsaws.py
+++ costar_models/python/costar_models/conditional_image_gan_jigsaws.py
@@ -73,8 +73,8 @@ class ConditionalImageGanJigsaws(ConditionalImageGan):
         y = Flatten()(OneHot(self.num_options)(option_in))
         y2 = Flatten()(OneHot(self.num_options)(option_in2))
         x = h
-        #tform = self._makeTransform(h_dim=(12,16))
-        tform = self._makeTransform(h_dim=(6,8))
+        tform = self._makeTransform(h_dim=(12,16))
+        #tform = self._makeTransform(h_dim=(6,8))
         x = tform([h0, h, y])
         x2 = tform([h0, x, y2])
         image_out, image_out2 = decoder([x]), decoder([x2])
