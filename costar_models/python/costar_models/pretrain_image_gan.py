7eac83abdc85d920fd225b917a5756102f89a739
Chris Paxton <cpaxton3@jhu.edu>
2018-01-23 15:13:43 -0800

    slightly simpler models

--- costar_models/python/costar_models/pretrain_image_gan.py
+++ costar_models/python/costar_models/pretrain_image_gan.py
@@ -99,10 +99,11 @@ class PretrainImageGan(RobotMultiPredictionSampler):
         dr = self.dropout_rate
         dr = 0
         
-        x = AddConv2D(img, 64, [4,4], 1, dr, "same", lrelu=True, bn=False)
-        x0 = AddConv2D(img0, 64, [4,4], 1, dr, "same", lrelu=True, bn=False)
-        x = Add()([x, x0])
-        x = AddConv2D(x, 64, [4,4], 2, dr, "same", lrelu=True, bn=True)
+        #x = AddConv2D(img, 64, [4,4], 1, dr, "same", lrelu=True, bn=False)
+        #x0 = AddConv2D(img0, 64, [4,4], 1, dr, "same", lrelu=True, bn=False)
+        #x = Add()([x, x0])
+        x = Concatenate(axis=-1)([img0, img])
+        x = AddConv2D(x, 64, [4,4], 2, dr, "same", lrelu=True, bn=False)
         x = AddConv2D(x, 128, [4,4], 2, dr, "same", lrelu=True, bn=True)
         x = AddConv2D(x, 256, [4,4], 2, dr, "same", lrelu=True, bn=True)
         x = AddConv2D(x, 1, [4,4], 1, 0., "same", activation="sigmoid",
