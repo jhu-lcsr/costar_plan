843123ffd3e6b21d5eed65ac315c4ea2bfb6501e
Chris Paxton <cpaxton3@jhu.edu>
2018-01-23 14:04:33 -0800

    wassersteining it up in the commands folder

--- commands/multi_pretrain_image_gan.sh
+++ commands/multi_pretrain_image_gan.sh
@@ -3,6 +3,9 @@ rosrun costar_models ctp_model_tool \
   --model pretrain_image_gan \
   --features multi \
   --data_file data.h5f \
-  --lr 0.0002  \
+  #--lr 0.0002  \
+  # Settings necessary for the Wasserstein GAN:
+  --lr 0.00005  \
+  --optimizer rmsprop \
   --steps_per_epoch 300 \
-  --dropout_rate 0.2 $1
+  --dropout_rate 0.1 $1
