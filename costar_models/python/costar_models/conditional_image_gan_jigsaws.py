61e48f82b3b5a6382361f1791ecbd0534f99ca3d
Chris Paxton <cpaxton3@jhu.edu>
2018-01-26 11:47:51 -0500

    tanh why not

--- costar_models/python/costar_models/conditional_image_gan_jigsaws.py
+++ costar_models/python/costar_models/conditional_image_gan_jigsaws.py
@@ -163,7 +163,7 @@ class ConditionalImageGanJigsaws(ConditionalImageGan):
         #x = AveragePooling2D(pool_size=(24,32))(x)
         x = x2
         x = Flatten()(x)
-	x = AddDense(x, 1, "linear", 0., output=True, bn=False)
+	x = AddDense(x, 1, "tanh", 0., output=True, bn=False)
         discrim = Model(ins, x, name="image_discriminator")
         self.lr *= 2.
         loss = wasserstein_loss if self.use_wasserstein else "binary_crossentropy"
