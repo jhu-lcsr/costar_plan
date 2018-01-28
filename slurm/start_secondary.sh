4555debca37668ba5063b609ec759493990754bb
Chris Paxton <cpaxton3@jhu.edu>
2018-01-27 18:07:55 -0800

    secondaries fix

--- slurm/start_secondary.sh
+++ slurm/start_secondary.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+noise_dim=0
+hd=true
+opt=adam
 for lr in 0.01 0.001 0.0002 0.0001
 do
   for loss in mae # logcosh
@@ -7,17 +10,12 @@ do
     # what do we do about skip connections?
     for skip in 0 # 1
     do
-      # Noise: add extra ones with no noise at all
-      for noise_dim in 0 # 1 8 32
+      for dr in 0.1 0.2 # 0. 0.3 0.4 0.5
       do
-        hd=true
-        for dr in 0.1 0.2 # 0. 0.3 0.4 0.5
+        for model in value next pose actor
         do
-          for model in value next pose actor
-          do
-            echo "Creating models with $opt $loss $skip $noise_dim $dr $model"
-            sbatch ctp_secondary.sh $lr $dr $opt $noise_dim $loss $model
-          done
+          echo "Creating models with $opt $loss $skip $noise_dim $dr $model"
+          sbatch ctp_secondary.sh $lr $dr $opt $noise_dim $loss $model
         done
       done
     done
