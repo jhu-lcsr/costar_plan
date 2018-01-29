76c02558ab430163506fa2628033bb5c617b7ef1
Yotam Barnoy <yotambarnoy@gmail.com>
2018-01-29 12:46:20 -0500

    slurm: add wasserstein support

--- slurm/start_ctp_gans.sh
+++ slurm/start_ctp_gans.sh
@@ -4,24 +4,30 @@ set -e
 set -x
 set -u
 
-for lr in 0.001 0.0002 0.0001
-do
+if [[ $# < 1 ]]; then
+  echo Usage: $0 wass/nowass
+  exit 1
+fi
+
+wass=$1
+
+lrs=0.001 0.0002 0.0001
+
+if [[ $wass == wass* ]]; then 
+  lrs=0.00005 0.0001 0.00002
+fi
+
+for lr in $lrs; do
   # just use the adam optimizer
-  for opt in adam
-  do
-    for loss in mae #logcosh
-    do
+  for opt in adam; do
+    for loss in mae; do #logcosh
     # what do we do about skip connections?
-    for skip in 0 # 1
-    do
+    for skip in 0; do # 1
       # Noise: add extra ones with no noise at all
-      for noise_dim in 0 # 1 8 32
-      do
-        hd=true
-        for dr in 0. 0.1 0.2 # 0.3 0.4 0.5
-        do
-          echo "starting LR=$lr, Dropout=$dr, optimizer=$opt, noise=$noise_dim"
-          sbatch ctp_gan.sh $lr $dr $opt $noise_dim $loss
+      for noise_dim in 0; do # 1 8 32
+        for dr in 0. 0.1 0.2; do # 0.3 0.4 0.5; do
+          echo "starting cpt_dec multi $wass LR=$lr, dr=$dr, opt=$opt, noise=$noise_dim"
+          sbatch ctp_gan.sh ctp_dec multi $lr $dr $opt $noise_dim $loss $wass
         done
       done
     done
