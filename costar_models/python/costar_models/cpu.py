abd64eb80f3b16e871d8889f5c0f3af9b5d8a8f5
Yotam Barnoy <yotambarnoy@gmail.com>
2018-01-31 16:56:26 -0500

    cpu: cleanup

--- costar_models/python/costar_models/cpu.py
+++ costar_models/python/costar_models/cpu.py
@@ -1,25 +1,20 @@
  
 def ConfigureGPU(args):
-    if 'cpu' in args and args['cpu']:
-        cpu = True
-    else:
-        cpu = False
+    cpu = True if 'cpu' in args and args['cpu'] else False
+
+    fraction = 1
     if 'gpu_fraction' in args and args['gpu_fraction']:
         fraction = args['gpu_fraction']
-    else:
-        fraction = 1.
 
     if fraction < 1. or cpu:
         import tensorflow as tf
         import keras.backend as K
         
         if cpu:
-            with tf.device('/cpu:0'):
-                config = tf.ConfigProto(
-                    device_count={'GPU': 0},
-                    gpu_options=gpu_options
-                )
-                sess = tf.Session(config=config)
+            config = tf.ConfigProto(
+                device_count={'GPU': 0}
+            )
+            sess = tf.Session(config=config)
         else:
             gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=fraction)
             sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
