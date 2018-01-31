a5c4f123813e8f5f0d7a9d161bcbbe151c13d351
Chris Paxton <cpaxton3@jhu.edu>
2018-01-31 18:50:45 -0500

    update h and ptimg

--- costar_models/python/costar_models/multi_hierarchical.py
+++ costar_models/python/costar_models/multi_hierarchical.py
@@ -358,14 +358,9 @@ class RobotMultiHierarchical(HierarchicalAgentBasedModel):
         '''
         img = Input(img_shape,name="img_encoder_in")
         bn = not disc and self.use_batchnorm
-        #img0 = Input(img_shape,name="img0_encoder_in")
-        dr = self.dropout_rate
+        dr = self.dropout_rate * 0.
         x = img
-        #x0 = img0
         x = AddConv2D(x, 32, [7,7], 1, 0., "same", lrelu=disc, bn=bn)
-        #x0 = AddConv2D(x0, 32, [7,7], 1, dr, "same", lrelu=disc, bn=bn)
-        #x = Concatenate(axis=-1)([x,x0])
-
         x = AddConv2D(x, 32, [5,5], 2, dr, "same", lrelu=disc, bn=bn)
         x = AddConv2D(x, 32, [5,5], 1, 0., "same", lrelu=disc, bn=bn)
         x = AddConv2D(x, 32, [5,5], 1, 0., "same", lrelu=disc, bn=bn)
@@ -420,6 +415,7 @@ class RobotMultiHierarchical(HierarchicalAgentBasedModel):
 
         x = rep
         dr = self.decoder_dropout_rate if self.hypothesis_dropout else 0
+        dr *= 0
         bn = self.use_batchnorm
         
         if self.use_spatial_softmax:
