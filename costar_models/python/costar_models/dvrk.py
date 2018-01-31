3ef2c9702a1710648e55e536a38309329ab925db
Chris Paxton <cpaxton3@jhu.edu>
2018-01-31 11:33:09 -0500

    less power in classifier

--- costar_models/python/costar_models/dvrk.py
+++ costar_models/python/costar_models/dvrk.py
@@ -37,24 +37,24 @@ def MakeJigsawsImageClassifier(model, img_shape, trainable = True):
     x = img
     x0 = img0
 
-    #x0 = AddConv2D(x0, 32, [5,5], 1, 0., "same", lrelu=disc, bn=bn)
-    x = AddConv2D(x, 32, [7,7], 1, 0., "same", lrelu=disc, bn=bn)
-    #x = Add()([x0, x])
+    x0 = AddConv2D(x0, 32, [5,5], 1, 0., "same", lrelu=disc, bn=bn)
+    x = AddConv2D(x, 32, [5,5], 1, 0., "same", lrelu=disc, bn=bn)
+    x = Add()([x0, x])
 
-    x = AddConv2D(x, 32, [5,5], 2, dr, "same", lrelu=disc, bn=bn)
-    x = AddConv2D(x, 32, [5,5], 1, 0., "same", lrelu=disc, bn=bn)
-    x = AddConv2D(x, 32, [5,5], 1, 0., "same", lrelu=disc, bn=bn)
-    x = AddConv2D(x, 64, [5,5], 2, dr, "same", lrelu=disc, bn=bn)
-    x = AddConv2D(x, 64, [5,5], 1, 0., "same", lrelu=disc, bn=bn)
-    x = AddConv2D(x, 64, [5,5], 2, dr, "same", lrelu=disc, bn=bn)
-    x = AddConv2D(x, 64, [5,5], 1, 0., "same", lrelu=disc, bn=bn)
-    x = AddConv2D(x, 64, [5,5], 2, dr, "same", lrelu=disc, bn=bn)
-    x = AddConv2D(x, 64, [5,5], 1, 0., "same", lrelu=disc, bn=bn)
-    x = AddConv2D(x, 128, [5,5], 2, dr, "same", lrelu=disc, bn=bn)
+    x = AddConv2D(x, 32, [3,3], 2, dr, "same", lrelu=disc, bn=bn)
+    x = AddConv2D(x, 32, [3,3], 1, 0., "same", lrelu=disc, bn=bn)
+    x = AddConv2D(x, 32, [3,3], 1, 0., "same", lrelu=disc, bn=bn)
+    x = AddConv2D(x, 64, [3,3], 2, dr, "same", lrelu=disc, bn=bn)
+    x = AddConv2D(x, 64, [3,3], 1, 0., "same", lrelu=disc, bn=bn)
+    x = AddConv2D(x, 64, [3,3], 2, dr, "same", lrelu=disc, bn=bn)
+    x = AddConv2D(x, 64, [3,3], 1, 0., "same", lrelu=disc, bn=bn)
+    x = AddConv2D(x, 64, [3,3], 2, dr, "same", lrelu=disc, bn=bn)
+    x = AddConv2D(x, 64, [3,3], 1, 0., "same", lrelu=disc, bn=bn)
+    x = AddConv2D(x, 128, [3,3], 2, dr, "same", lrelu=disc, bn=bn)
 
     #x = MaxPooling2D((3,4))(x)
     x = Flatten()(x)
-    #x = AddDense(x, 512, "lrelu", dr, output=True, bn=bn)
+    x = AddDense(x, 512, "lrelu", dr, output=True, bn=bn)
     x = AddDense(x, model.num_options, "softmax", 0., output=True, bn=False)
     image_encoder = Model([img0, img], x, name="classifier")
     if not trainable:
