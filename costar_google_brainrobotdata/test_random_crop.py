10774d0d814af40286c2576e9a7b07703aa26218
Ding <yding37@jhu.edu>
2018-02-06 17:55:23 -0500

    unit test passed

--- costar_google_brainrobotdata/test_random_crop.py
+++ costar_google_brainrobotdata/test_random_crop.py
@@ -63,12 +63,12 @@ class random_crop_test(tf.test.TestCase):
                 test_input = tf.random_uniform(input_size_tf)
                 test_input = tf.reshape(test_input, input_size_tf)
 
-                transform_full = rcp.random_projection_transform(input_size)
+                transform_full, random_features = rcp.random_projection_transform(input_size)
                 rcp_proj_full = rcp.transform_crop_and_resize_image(test_input, transform=transform_full)
 
-                transform = rcp.random_projection_transform(input_size, cropped_size_tf)
+                transform, random_features = rcp.random_projection_transform(input_size, cropped_size_tf)
                 rcp_proj = rcp.transform_crop_and_resize_image(
-                    test_input, transform=transform, size=cropped_size_tf)
+                    test_input, transform=transform, crop_shape=cropped_size_tf)
 
                 return rcp_proj, transform, rcp_proj_full, transform_full
 
