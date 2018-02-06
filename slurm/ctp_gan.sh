eec049ea86f3d78679e80878e02e760c84e99899
Yotam Barnoy <yotambarnoy@gmail.com>
2018-02-06 16:16:35 -0500

    slurm/ctp_gan: allow running not on marcc

--- slurm/ctp_gan.sh
+++ slurm/ctp_gan.sh
@@ -7,10 +7,15 @@
 #SBATCH --mem=8G
 #SBATCH --mail-type=end
 
+# Check if running on marcc
+if [[ -z ${SLURM_JOB_ID+x} ]]; then marcc=false; else marcc=true; fi
 
-echo "Running $@ on $SLURMD_NODENAME ..."
-
-module load tensorflow/cuda-8.0/r1.3
+if $marcc; then
+  echo "Running $@ on $SLURMD_NODENAME ..."
+  module load tensorflow/cuda-8.0/r1.3
+else
+  echo "Not running on Marcc"
+fi
 
 OPTS=$(getopt -o '' --long lr:,dr:,opt:,noisedim:,loss:,wass,no_wass,noise,retrain,encoder,gan_encoder,load_model -n ctp_gan -- "$@")
 
@@ -59,7 +64,6 @@ features="$2"
 [[ $dataset == '' ]] && echo 'Dataset is mandatory!' && exit 1
 [[ $features == '' ]] && echo 'Features are mandatory!' && exit 1
 
-
 if $wass; then wass_dir=wass; else wass_dir=nowass; fi
 if $use_noise; then noise_dir=noise; else noise_dir=nonoise; fi
 
@@ -68,12 +72,15 @@ MODELDIR="$HOME/.costar/${dataset}_${lr}_${optimizer}_${dropout}_${noise_dim}_${
 [[ ! -d $MODELDIR ]] && mkdir -p $MODELDIR
 touch $MODELDIR/$SLURM_JOB_ID
 
-# Handle different Marcc layouts
-data_dir=$HOME/work/$dataset
-[[ ! -d $data_dir ]] && data_dir=$HOME/work/dev_yb/$dataset
+if $marcc; then
+  # Handle different Marcc layouts
+  data_dir=$HOME/work/$dataset
+  [[ ! -d $data_dir ]] && data_dir=$HOME/work/dev_yb/$dataset
+else
+  data_dir=$dataset
+fi
 
 if [[ $features == husky ]]; then data_suffix=npz; else data_suffix=h5f; fi
-
 data_dir=${data_dir}.${data_suffix}
 
 if $wass; then wass_cmd='--wasserstein'; else wass_cmd=''; fi
@@ -81,9 +88,15 @@ if $use_noise; then use_noise_cmd='--use_noise'; else use_noise_cmd=''; fi
 if $retrain; then retrain_cmd='--retrain'; else retrain_cmd=''; fi
 if $load_model; then load_cmd='--load_model'; else load_cmd=''; fi
 
+if $marcc; then
+  cmd_prefix="$HOME/costar_plan/costar_models/scripts/"
+else
+  cmd_prefix='rosrun costar_models '
+fi
+
 if $train_image_encoder; then
   echo "Training discriminator"
-  $HOME/costar_plan/costar_models/scripts/ctp_model_tool \
+  ${cmd_prefix}ctp_model_tool \
     --features $features \
     -e 100 \
     --model discriminator \
@@ -99,7 +112,7 @@ if $train_image_encoder; then
     $load_cmd
 
   echo "Training non-gan image encoder"
-  $HOME/costar_plan/costar_models/scripts/ctp_model_tool \
+  ${cmd_prefix}ctp_model_tool \
     --features $features \
     -e 100 \
     --model pretrain_image_encoder \
@@ -116,7 +129,7 @@ if $train_image_encoder; then
 fi
 if $train_gan_image_encoder; then
   echo "Training encoder gan"
-  $HOME/costar_plan/costar_models/scripts/ctp_model_tool \
+  ${cmd_prefix}ctp_model_tool \
     --features $features \
     -e 300 \
     --model pretrain_image_gan \
@@ -135,7 +148,7 @@ if $train_gan_image_encoder; then
 fi
 
 echo "Training conditional gan"
-$HOME/costar_plan/costar_models/scripts/ctp_model_tool \
+  ${cmd_prefix}ctp_model_tool \
   --features $features \
   -e 100 \
   --model conditional_image_gan \
