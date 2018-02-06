bbdb73e0e018f037049f5ff2154f393fb1bcd305
Yotam Barnoy <yotambarnoy@gmail.com>
2018-02-06 13:05:17 -0500

    slurm: added options to ctp_gan

--- slurm/ctp_gan.sh
+++ slurm/ctp_gan.sh
@@ -10,40 +10,69 @@
 
 echo "Running $@ on $SLURMD_NODENAME ..."
 
-module load tensorflow/cuda-8.0/r1.3 
+module load tensorflow/cuda-8.0/r1.3
 
-export train_image_encoder=false
-export train_gan_image_encoder=false
+OPTS=$(getopt -o d:f: --long dataset:,features:,lr:,dr:,opt:,noisedim:,loss:,wass,no-wass,noise,retrain,train-img-encoder,train-gan-encoder -n ctp_gan -- "$@")
 
-export dataset=$1
-export features=$2
-export learning_rate=$3
-export dropout=$4
-export optimizer=$5
-export noise_dim=$6
-export loss=$7
-export wass=$8 # 'wass
-export use_noise=$9
-export MODELDIR="$HOME/.costar/${dataset}_${learning_rate}_${optimizer}_${dropout}_${noise_dim}_${loss}_${wass}_${use_noise}"
+train_image_encoder=false
+train_gan_image_encoder=false
+dataset=''
+features=''
+lr=0.001
+dropout=0.1
+optimizer=adam
+noise_dim=1
+loss=mae
+wass=false
+use_noise=false
+retrain=false
+
+while true; do
+  case "$1" in
+    -d | --dataset) dataset="$2"; shift 2 ;;
+    -f | --features) features="$2"; shift 2 ;;
+    --lr) lr="$2"; shift 2 ;;
+    --dr) dropout="$2"; shift 2 ;;
+    --opt) optimizer="$2"; shift 2 ;;
+    --noisedim) noise_dim="$2"; shift 2 ;;
+    --noise) use_noise=true; shift ;;
+    --loss) loss="$2"; shift 2 ;;
+    --wass) wass=true; shift ;;
+    --no-wass) wass=false; shift ;;
+    --retrain) retrain=true; shift ;;
+    --train-img-encoder) train_image_encoder=true; shift ;;
+    --train-gan-encoder) train_gan_encoder=true; shift ;;
+    --) shift; break ;;
+    *) echo "Internal error!" ; exit 1 ;;
+  esac
+done
+
+[[ $dataset != '' ]] && echo 'Dataset is mandatory!' && exit 1
+[[ $features != '' ]] && echo 'Features are mandatory!' && exit 1
+
+
+MODELDIR="$HOME/.costar/${dataset}_${lr}_${optimizer}_${dropout}_${noise_dim}_${loss}_${wass}_${use_noise}"
+
 touch $MODELDIR/$SLURM_JOB_ID
 
 # Handle different Marcc layouts
 data_dir=$HOME/work/$dataset
-if [[ ! -d $data_dir ]]; then
-  data_dir=$HOME/work/dev_yb/$dataset
-fi
-if [[ $features == husky ]]; then
-  data_dir=${data_dir}.npz
-else
-  data_dir=${data_dir}.h5f
-fi
+[[ ! -d $data_dir ]] && data_dir=$HOME/work/dev_yb/$dataset
+
+data_suffix=h5f
+[[ $features == husky ]] && data_suffix=npz
+
+data_dir=${data_dir}.${data_suffix}
 
 wass_cmd=''
-if [[ $wass == wass* ]]; then wass_cmd='--wasserstein'; fi
+$wass && wass_cmd='--wasserstein'
 
 use_noise_cmd=''
-if [[ $use_noise == true ]]; then use_noise_cmd='--use_noise'; fi
-	
+$use_noise && use_noise_cmd='--use_noise'
+
+retrain_cmd=''
+$retrain && retrain_cmd='--retrain'
+
 if $train_image_encoder; then
   echo "Training discriminator"
   $HOME/costar_plan/costar_models/scripts/ctp_model_tool \
@@ -51,7 +80,7 @@ if $train_image_encoder; then
     -e 100 \
     --model discriminator \
     --data_file $data_dir \
-    --lr $learning_rate \
+    --lr $lr \
     --dropout_rate $dropout \
     --model_directory $MODELDIR/ \
     --optimizer $optimizer \
@@ -66,7 +95,7 @@ if $train_image_encoder; then
     -e 100 \
     --model pretrain_image_encoder \
     --data_file $data_dir \
-    --lr $learning_rate \
+    --lr $lr \
     --dropout_rate $dropout \
     --model_directory $MODELDIR/ \
     --optimizer $optimizer \
@@ -76,13 +105,13 @@ if $train_image_encoder; then
     --batch_size 64
 fi
 if $train_gan_image_encoder; then
-  echo "Training encoder gan: no wasserstein"
+  echo "Training encoder gan"
   $HOME/costar_plan/costar_models/scripts/ctp_model_tool \
     --features $features \
     -e 100 \
     --model pretrain_image_gan \
     --data_file $data_dir \
-    --lr $learning_rate \
+    --lr $lr \
     --dropout_rate $dropout \
     --model_directory $MODELDIR/ \
     --optimizer $optimizer \
@@ -100,7 +129,7 @@ $HOME/costar_plan/costar_models/scripts/ctp_model_tool \
   -e 100 \
   --model conditional_image_gan \
   --data_file $data_dir \
-  --lr $learning_rate \
+  --lr $lr \
   --dropout_rate $dropout \
   --model_directory $MODELDIR/ \
   --optimizer $optimizer \
@@ -109,5 +138,6 @@ $HOME/costar_plan/costar_models/scripts/ctp_model_tool \
   --loss $loss \
   --gan_method gan \
   --batch_size 64 \
-  $wass_cmd
+  $wass_cmd \
+  $use_noise_cmd
 
--- slurm/start_ctp_gans.sh
+++ slurm/start_ctp_gans.sh
@@ -10,18 +10,41 @@ cd "$SCRIPT_DIR"/../costar_models/python
 python setup.py install --user
 cd -
 
+OPTS=$(getopt --long retrain,load_model -n start_ctp_gans -- "$@")
+
+retrain=false
+load_model=false
 lr=0.0001
 dr=0.1
-opt=rmsprop
+opt=adam
 noise_dim=4
 wass=wass
 loss=mae
-lr=0.0001
 
-for w in wass nowass; do
-  for t in true false; do
-    sbatch "$SCRIPT_DIR"/ctp_gan.sh ctp_dec multi $lr $dr $opt $noise_dim $loss $w $t
-    sbatch "$SCRIPT_DIR"/ctp_gan.sh husky_data husky $lr $dr $opt $noise_dim $loss $w $t
-    sbatch "$SCRIPT_DIR"/ctp_gan.sh suturing_data2 jigsaws $lr $dr $opt $noise_dim $loss $w $t
+while true; do
+  case "$1" in
+    --retrain) retrain=true; shift ;;
+    --load-model) load_model=true; shift ;;
+    --) shift; break ;;
+    *) echo "Internal error!" ; exit 1 ;;
+  esac
+done
+
+retrain_cmd=''
+$retrain && retrain_cmd='--retrain'
+
+for wass in true false; do
+
+  wass_cmd=''
+  $wass && wass_cmd='--wass'
+  $wass && opt=rmsprop
+
+  for use_noise in true false; do
+    noise_cmd=''
+    $use_noise && noise_cmd='--noise'
+
+    sbatch "$SCRIPT_DIR"/ctp_gan.sh ctp_dec        multi   --lr $lr --dr $dr --opt $opt $wass_cmd $noise_cmd $retrain_cmd
+    sbatch "$SCRIPT_DIR"/ctp_gan.sh husky_data     husky   --lr $lr --dr $dr --opt $opt $wass_cmd $noise_cmd $retrain_cmd
+    sbatch "$SCRIPT_DIR"/ctp_gan.sh suturing_data2 jigsaws --lr $lr --dr $dr --opt $opt $wass_cmd $noise_cmd $retrain_cmd
   done
 done
