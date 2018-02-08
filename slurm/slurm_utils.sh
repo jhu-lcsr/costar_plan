327c4be683c970012ddc0222bd172a7b8f7452e8
Yotam Barnoy <yotambarnoy@gmail.com>
2018-02-08 18:52:09 -0500

    slurm: util to clean up dead out files

--- slurm/slurm_utils.sh
+++ slurm/slurm_utils.sh
@@ -45,6 +45,23 @@ function feh_job() {
         feh $dir/debug
 }
 
+function clean_job_outs() {
+  declare -A job_table
+  jobs=$(sqme | tail -n+3 | awk '{print $1}')
+  for j in $jobs; do job_table[$j]=true; done
+  files="./slurm-*.out"
+  for f in $files; do
+    fjob=${f/\.\/slurm-/}
+    fjob=${fjob/\.out/}
+    if [[ ${job_table[$fjob]} == true ]]; then
+      echo Not deleting $f
+    else
+      echo Deleting $f
+      rm $f
+    fi
+  done
+}
+
 
 
 
