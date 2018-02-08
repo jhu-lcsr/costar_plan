d765e0c88b9b8b7b12ff79fc98b90e92d0fffe6e
Yotam Barnoy <yotambarnoy@gmail.com>
2018-02-08 12:49:45 -0500

    slurm: add running time to running_jobs

--- slurm/slurm_utils.sh
+++ slurm/slurm_utils.sh
@@ -2,30 +2,46 @@
 # Source this file to get these utility functions
 
 function running_jobs() {
-	jobs=$(sqme | tail -n+3 | awk '{print $1}')
+  jobs=$(sqme | tail -n+3 | awk '{print $1","$6}')
 
-	results=''
-	for job in $jobs; do
-		results="$results $(find $HOME/.costar -name $job)"
-	done
+  declare -A jobtimes
+  results=''
+  for j in $jobs; do
+    job="${j%,*}"
+    time="${j##*,}"
+    jobtimes[$job]=$time
+    results="$results $(find $HOME/.costar -name $job)"
+  done
 
-	for r in $results; do
-		job="${r##*/}"
-		dir="${r%/*}"
-		dir2="${dir##*/}"
-		echo "$job $dir2"
-	done
+  for r in $results; do
+    job="${r##*/}"
+    dir="${r%/*}"
+    dir="${dir##*/}"
+    echo "$job ${jobtimes[$job]} $dir"
+  done
 }
 
 function find_job() {
-	find $HOME/.costar -name '*'$1
+        find $HOME/.costar -name '*'$1
+}
+
+function job_dir() {
+        dir="$(find_job $1)"
+        dir="${dir%/*}"
+        echo $dir
 }
 
 function cd_job() {
-	dir="$(find $HOME/.costar -name '*'$1)"
-	dir="${dir%/*}"
-	cd $dir
+  dir="$(job_dir $1)"
+        cd $dir
 }
+
+function feh_job() {
+  dir="$(job_dir $1)"
+        feh $dir/debug
+}
+
+
 
 
 
