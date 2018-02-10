ebd741868b8d548db4e1811808038112c40296e7
Chris Paxton <cpaxton3@jhu.edu>
2018-02-09 21:08:17 -0500

    decrease wt on classifier

--- costar_models/python/costar_models/conditional_image_jigsaws.py
+++ costar_models/python/costar_models/conditional_image_jigsaws.py
@@ -89,7 +89,7 @@ class ConditionalImageJigsaws(ConditionalImage):
         if self.no_disc:
             disc_wt = 0.
         else:
-            disc_wt = 1e-3
+            disc_wt = 1e-4
         if self.no_disc:
             model = Model(ins + [prev_option_in],
                     [image_out, image_out2,])
