transform_and_crop_coordinate figured out tricky rotation bug

costar_google_brainrobotdata/cornell_grasp_dataset_reader.py
@@ -93,10 +93,10 @@ flags.DEFINE_boolean('random_crop', True,
                         that you crop images to the same size during both training
                         and test time.
                      """)
-flags.DEFINE_integer('resize_width', 320,
-                     """Width to resize images before prediction, if enabled.""")
 flags.DEFINE_integer('resize_height', 240,
                      """Height to resize images before prediction, if enabled.""")
+flags.DEFINE_integer('resize_width', 320,
+                     """Width to resize images before prediction, if enabled.""")
 flags.DEFINE_boolean('resize', True,
                      """resize will resize the input images to the desired dimensions specified by the
                         resize_width and resize_height flags. It is suggested that an exact factor of 2 be used
@@ -378,7 +378,7 @@ def parse_and_preprocess(
         # perform image augmentation
         # TODO(ahundt) add scaling and use that change to augment height and width parameters
         transform, random_features = rcp.random_projection_transform(
-            K.shape(image), crop_shape, scale=False)
+            K.shape(image), crop_shape, scale=True, rotation=K.constant([0.0, 1.0], 'float32'))
         image, preprocessed_grasp_center_coordinate = rcp.transform_crop_and_resize_image(
             image, crop_shape=crop_shape, resize_shape=output_shape,
             transform=transform, coordinate=grasp_center_coordinate)
@@ -393,6 +393,7 @@ def parse_and_preprocess(
         image, preprocessed_grasp_center_coordinate = rcp.transform_crop_and_resize_image(
             image, crop_shape=crop_shape, central_crop=True,
             resize_shape=output_shape, coordinate=grasp_center_coordinate)
+        grasp_center_rotation_theta = K.constant(0.0, 'float32')
 
     feature['image/transformed'] = image
 
@@ -565,6 +566,7 @@ def visualize_redundant_example(features_dicts, showTextBox=False):
 
     preprocessed_examples = []
     for example in features_dicts:
+        print('original example bbox/theta: ' + str(example['bbox/theta']))
         if ('bbox/preprocessed/cy_cx_normalized_2' in example):
             # Reverse the preprocessing so we can visually compare correctness
             decoded_example = copy.deepcopy(example)
@@ -592,7 +594,10 @@ def visualize_redundant_example(features_dicts, showTextBox=False):
             decoded_example['bbox/height'] = example['bbox/preprocessed/height']
             decoded_example['bbox/cy'] = example['bbox/preprocessed/cy']
             decoded_example['bbox/cx'] = example['bbox/preprocessed/cx']
-            decoded_example['bbox/theta'] = example['bbox/preprocessed/theta']
+            if 'random_projection_transform' in example:
+                print('random_projection_transform:' + str(example['random_projection_transform']))
+                print('random_rotation: ' + str(example['random_rotation']) )
+                print('bbox/preprocessed/theta: ' + str(example['bbox/preprocessed/theta']))
             preprocessed_examples.append(decoded_example)
 
     img = example['image/decoded']
@@ -646,8 +651,12 @@ def visualize_redundant_example(features_dicts, showTextBox=False):
         if False:
         # if showTextBox:
             axs[h, w].text(
-                    cx, cy,
-                    success_str, size=10, rotation=-np.rad2deg(example['bbox/theta']),
+                    int(cx[0]), int(cy[0]),
+                    success_str, size=10,
+                    # see the gripper angle
+                    rotation=float(np.rad2deg(example['bbox/theta'])),
+                    # this next one is just to see if random rotation matches actual image rotation
+                    # rotation=float(np.rad2deg(example['random_rotation'])),
                     ha="right", va="top",
                     bbox=dict(boxstyle="square",
                               ec=(1., 0.5, 0.5),
@@ -657,7 +666,7 @@ def visualize_redundant_example(features_dicts, showTextBox=False):
                     )
             z += 1
 
-        axs[h, w].scatter(cy, cx, zorder=2, alpha=0.5, lw=2)
+        axs[h, w].scatter(cx, cy, zorder=2, alpha=0.5, lw=2)
         # bbox/x_i, y_i are not calculated according to preprocess, so use rectangle here
         # axs[h, w].add_patch(patches.Rectangle((example['bbox/cx'], example['bbox/cy']),
         #                                       example['bbox/width'], example['bbox/height'],

costar_google_brainrobotdata/random_crop.py
@@ -73,7 +73,8 @@ def crop_images(image_list, offset, size, name=None, verbose=0):
         else:
             size = ops.convert_to_tensor(size, dtype=dtypes.int32, name="size")
             image_list = ops.convert_to_tensor(image_list, name="image")
-            offset = ops.convert_to_tensor(offset, dtype=dtypes.int32, name="offset")
+            if not tf.contrib.framework.is_tensor(offset):
+                offset = ops.convert_to_tensor(offset, dtype=dtypes.int32, name="offset")
             if verbose > 0:
                 print("crop_images offset:", offset, 'size', size, 'img_list_shape', image_list.shape)
                 offset = tf.Print(offset, [offset, size, image_list.shape])
@@ -235,7 +236,7 @@ def transform_and_crop_coordinate(coordinate, transform=None, offset=None, inver
     # Arguments
 
         coordinate: A 2D image coordinate.
-        transform: A 3x3 homogenous 2D image transform matrix.
+        transform: A 3x3 homogenous 2D image transform matrix stored in shape [1, 8].
         offset: A crop offset which is the location of (0,0) in the post-crop image.
 
     # Returns
@@ -250,15 +251,18 @@ def transform_and_crop_coordinate(coordinate, transform=None, offset=None, inver
         # projection_matrix = tf.contrib.image._flat_transforms_to_matrices(transform)
 
         if transform is not None:
+            # Very important: most TF code expects y, x coordinates
+            # but the projection matrix is generated uing x, y coordinates
+            # so we swap it from (y,x) to (x,y) here.
             if not tf.contrib.framework.is_tensor(coordinate):
                 coordinate = tf.transpose(tf.convert_to_tensor(
-                    coordinate[0],
                     coordinate[1],
+                    coordinate[0],
                     1
                 ))
             else:
-                coordinate = tf.stack([tf.reshape(coordinate[0], (1,)),
-                                       tf.reshape(coordinate[1], (1,)),
+                coordinate = tf.stack([tf.reshape(coordinate[1], (1,)),
+                                       tf.reshape(coordinate[0], (1,)),
                                        tf.constant([1], tf.float32)], axis=-1)
             coordinate = tf.transpose(coordinate)
             if inverse:
@@ -266,11 +270,19 @@ def transform_and_crop_coordinate(coordinate, transform=None, offset=None, inver
             projection_matrix = tf.squeeze(projection_matrix)
             coordinate = tf.matmul(projection_matrix,
                                    coordinate)
-            coordinate = coordinate[:2]
+            coordinate = tf.squeeze(tf.transpose(coordinate))
+            # coordinate -= transform_offset
+            coordinate = tf.stack([coordinate[1], coordinate[0]])
+            # Very important: most TF code expects y, x coordinates
+            # but the projection matrix is generated uing x, y coordinates
+            # so we swap it back from (x,y) to (y,x) here.
         if offset is not None:
             if isinstance(offset, list):
                 offset = tf.constant([[offset[0]], [offset[1]]], tf.float32)
-            coordinate = coordinate - offset
+
+            coordinate = tf.Print(coordinate, [coordinate, offset], 'transform_and_crop_pre_offset: coodinate, offset')
+            coordinate = coordinate - tf.cast(offset[:2], tf.float32)
+            coordinate = tf.Print(coordinate, [coordinate, offset], 'transform_and_crop_post_offset: coodinate, offset')
     return coordinate
 
 
@@ -290,6 +302,7 @@ def resize_coordinate(coordinate, input_shape, output_shape, name=None):
         else:
             proportional_dimension_change = output_shape / input_shape[:2]
 
+        coordinate = tf.Print(coordinate, [coordinate, proportional_dimension_change], 'coordinate proportional_dim')
         resized_coordinate = tf.squeeze(coordinate) * proportional_dimension_change
     return resized_coordinate
 
@@ -330,7 +343,7 @@ def transform_crop_and_resize_image(
 
        cropped_image if coordinate is None, otherwise [cropped_image, new_coordinate]
     """
-    with tf.name_scope(name, "transform_and_crop_image",
+    with tf.name_scope(name, "transform_crop_and_resize_image",
                        [image]) as name:
         if crop_shape is None and offset is not None:
             raise ValueError('If crop_shape is None offset must also be None.')
@@ -347,7 +360,7 @@ def transform_crop_and_resize_image(
             else:
                 # in this case the random part of the
                 # random crop is built into the transform
-                offset = [0, 0, 0]
+                offset = tf.constant([0, 0, 0], tf.int32)
 
             image = crop_images(image, offset, crop_shape)
 