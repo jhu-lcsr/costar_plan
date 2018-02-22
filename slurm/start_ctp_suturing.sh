baa46daba7163723f67cfd9fe71eacfd3f1ba7e2
Chris Paxton <cpaxton3@jhu.edu>
2018-02-22 18:51:10 -0500

    adding to scripts: check use ssm

--- slurm/start_ctp_suturing.sh
+++ slurm/start_ctp_suturing.sh
@@ -19,12 +19,15 @@ do
       # Noise: add extra ones with no noise at all
       for noise_dim in 0 # 1 8 32
       do
+        for use_ssm in 0 1
+        do
         hd=true
         use_disc=true
-        for dr in 0.1 0.2 0. 0.3 #0.4 0.5
+        for dr in 0.1 #0.2 0. #0.3 #0.4 0.5
         do
           echo "starting LR=$lr, Dropout=$dr, optimizer=$opt, noise=$noise_dim"
-          sbatch ctp_suturing.sh $lr $dr $opt $noise_dim $loss false $use_disc $skip
+          sbatch ctp_suturing.sh $lr $dr $opt $noise_dim $loss false $use_disc $skip $use_ssm
+          done
         done
       done
     done
