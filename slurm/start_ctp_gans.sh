99ba511a3074299501bd13d385a94e394c8eda68
Yotam Barnoy <yotambarnoy@gmail.com>
2018-02-25 22:50:17 -0500

    slurm/gan: Update with new options

--- slurm/start_ctp_gans.sh
+++ slurm/start_ctp_gans.sh
@@ -10,7 +10,7 @@ cd "$SCRIPT_DIR"/../costar_models/python
 python setup.py install --user
 cd -
 
-OPTS=$(getopt -o '' --long retrain,load_model,gan_encoder,skip_encoder,suffix:,no_resume,epochs1:,epochs2:,skip_cond,no_husky,no_jigsaws,no_ctp,common_encoder -n start_ctp_gans -- "$@")
+OPTS=$(getopt -o '' --long retrain,load_model,gan_encoder,no_gan_encoder,skip_encoder,suffix:,no_resume,epochs1:,epochs2:,skip_cond,no_husky,no_jigsaws,no_ctp,no_wass,common_encoder -n start_ctp_gans -- "$@")
 
 [[ $? != 0 ]] && echo "Failed parsing options." && exit 1
 
@@ -35,12 +35,13 @@ skip_cond_cmd=''
 run_ctp=true
 run_husky=true
 run_jigsaws=true
+run_wass=true
 common_encoder=false # Use an encoder in a separate location
 
 while true; do
   case "$1" in
     --retrain) retrain=true; shift ;;
-    --encoder) gan_encoder=false; shift ;;
+    --no_gan_encoder) gan_encoder=false; shift ;;
     --gan_encoder) gan_encoder=true; shift ;;
     --skip_encoder) skip_encoder=true; shift ;;
     --common_encoder) common_encoder=true; skip_encoder=true; shift ;;
@@ -53,6 +54,7 @@ while true; do
     --no_ctp) run_ctp=false; shift ;;
     --no_husky) run_husky=false; shift ;;
     --no_jigsaws) run_jigsaws=false; shift ;;
+    --no_wass) run_wass=false; shift ;;
     --) shift; break ;;
     *) echo "Internal error!" ; exit 1 ;;
   esac
@@ -67,18 +69,22 @@ if $resume; then resume_cmd=''; else resume_cmd='--no_resume'; fi
 # Check for the common encoder location
 if $common_encoder; then  enc_dir_cmd='--enc_dir ~/.costar/pretrain_codec'; else enc_dir_cmd=''; fi
 
-for wass_cmd in --wass ''; do
-  if [[ $wass_cmd == '--wass' ]]; then opt=rmsprop; else opt=adam; fi
+if $run_wass; then wass_list='true false'; else wass_list=false; fi
+for do_wass in $wass_list; do
+  if $do_wass; then wass_cmd='--wass'; else wass_cmd=''; fi
+  if $do_wass; then opt=rmsprop; else opt=adam; fi
   for noise_cmd in --noise ''; do
-    for gan_cmd in --gan_encoder ''; do
+    if ! $gan_encoder; then enc_list='false'; else enc_list='true false'; fi
+    for do_gan_enc in $enc_list; do
+      if $do_gan_enc; then enc_cmd='--gan_encoder'; else enc_cmd=''; fi
       # double the epochs for gan encoder
-      [[ $gan_cmd == '--gan_encoder' ]] && epochs1=$(($epochs1 * 2))
+      $do_gan_enc && epochs1=$(($epochs1 * 2))
 
       for retrain_cmd in ''; do # --retrain
         function call() {
           sbatch "$SCRIPT_DIR"/ctp_gan.sh "$1" "$2" --lr $lr --dr $dr \
             --opt $opt --epochs1 $epochs1 --epochs2 $epochs2 \
-            $wass_cmd $noise_cmd $retrain_cmd $gan_cmd \
+            $wass_cmd $noise_cmd $retrain_cmd $enc_cmd \
             $load_cmd $skip_cmd $suffix_cmd $resume_cmd $skip_cond_cmd \
             $enc_dir_cmd
         }
@@ -87,7 +93,7 @@ for wass_cmd in --wass ''; do
         $run_jigsaws && call suturing_data2 jigsaws
       done
 
-      [[ $gan_cmd == '--gan_encoder' ]] && epochs1=$(($epochs1 / 2))
+      $do_gan_enc && epochs1=$(($epochs1 / 2))
     done
   done
 done
