0bd979f06c84a7397693631f70cb7a0d8f2e5cfb
Chris Paxton <cpaxton3@jhu.edu>
2018-03-05 10:55:46 -0500

    fix issue in DVRK code w/ dropout

--- costar_models/python/costar_models/dvrk.py
+++ costar_models/python/costar_models/dvrk.py
@@ -158,7 +158,7 @@ def MakeJigsawsTransform(model, h_dim=(12,16), perm_drop=False):
         x = Lambda(_ssm,name="encoder_spatial_softmax")(x)
         x = Concatenate(axis=-1)([x, y])
         x = AddDense(x, int(h_dim[0] * h_dim[1] * 64/16),
-              activation_fn, self.dropout_rate, constraint=None, bn=False, output=False)
+              activation_fn, model.dropout_rate, constraint=None, bn=False, output=False)
         x = Reshape([int(h_dim[0]/4), int(h_dim[1]/4), 64])(x)
     else:
         x = AddConv2D(x, 64, [5,5], 2, **kwargs_dr0)
