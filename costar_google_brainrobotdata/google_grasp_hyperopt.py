9c938eb4be2a8cb5336824a822e218d708e63f5f
Andrew Hundt <ATHundt@gmail.com>
2018-04-08 20:56:36 -0400

    increment seeds to restart hyperopt

--- costar_google_brainrobotdata/google_grasp_hyperopt.py
+++ costar_google_brainrobotdata/google_grasp_hyperopt.py
@@ -38,7 +38,7 @@ def main(_):
     batch_size = 4
     min_top_block_filter_multiplier = 6
     feature_combo_name = None
-    seed = 6
+    seed = 7
 
     # TODO(ahundt) hyper optimize more input feature_combo_names (ex: remove sin theta cos theta), optimizers, etc
     # continuous variables and then discrete variables
