11d3c47d4a79f2f7983a1d973c1753edd26fc4ab
Andrew Hundt <athundt@gmail.com>
2018-05-05 00:58:33 -0400

    view_convert_dataset.py can now display depth and rgb

--- ctp_integration/scripts/view_convert_dataset.py
+++ ctp_integration/scripts/view_convert_dataset.py
@@ -16,7 +16,7 @@ import numpy as np
 import h5py 
 import matplotlib.pyplot as plt
 # https://github.com/tanyaschlusser/array2gif
-from array2gif import write_gif
+# from array2gif import write_gif
 
 # progress bars https://github.com/tqdm/tqdm
 # import tqdm without enforcing it as a dependency
@@ -32,6 +32,12 @@ except ImportError:
 from costar_models.datasets.image import JpegToNumpy
 from costar_models.datasets.image import ConvertImageListToNumpy
 import pygame
+import io
+from PIL import Image
+import moviepy
+import moviepy.editor as mpye
+# import skimage
+
 
 
 
@@ -64,8 +70,18 @@ def _parse_args():
     parser.add_argument("--ignore_failure", type=bool, default=False, help='skip grasp failure cases')
     parser.add_argument("--ignore_success", type=bool, default=False, help='skip grasp success cases')
     parser.add_argument("--preview", type=bool, default=False, help='pop open a preview window to view the video data')
-    parser.add_argument("--depth", type=bool, default=False, help='process depth data instead of rgb data')
+    parser.add_argument("--depth", type=bool, default=True, help='process depth data')
+    parser.add_argument("--rgb", type=bool, default=True, help='process rgb data')
+    parser.add_argument("--fps", type=int, default=10, help='framerate to process images in frames per second')
+    parser.add_argument("--matplotlib", type=bool, default=False, 
+                        help='preview data with matplotlib, slower but you can do pixel lookups')
     return vars(parser.parse_args())
+
+def draw_matplotlib(depth_images, fps):
+    for image in tqdm(depth_images):
+        plt.imshow(image)
+        plt.pause(1.0 / fps)
+        plt.draw()
 
 def main(args,root="root"):
 
@@ -79,7 +95,9 @@ def main(args,root="root"):
     for filename in progress_bar: 
         if filename.startswith('.') or '.h5' not in filename:
             continue
-        
+        if 'error' in filename:
+            progress_bar.write('Skipping example containing errors')
+            continue
         if args['ignore_failure'] and 'failure' in filename:
                 continue
         if args['ignore_success'] and 'success' in filename:
@@ -94,18 +112,34 @@ def main(args,root="root"):
 
         data = h5py.File(example_filename,'r')
 
-        image_identifier = 'image'
+        fps=args['fps']
+
         if args['depth']:
-            image_identifier = 'depth_image'
-        images = list(data[image_identifier])
-        # images = ConvertImageListToNumpy(np.squeeze(images), format='list', data_format='NCHW')
-        images = ConvertImageListToNumpy(np.squeeze(images), format='list')
-        # images = ConvertImageListToNumpy(images)
-        # progress_bar.write('numpy gif data shape: ' + str(images.shape))
+            depth_images = list(data['depth_image'])
+            depth_images = ConvertImageListToNumpy(np.squeeze(depth_images), format='list')
+            if args['matplotlib']:
+                draw_matplotlib(depth_images, fps)
+            depth_clip = mpye.ImageSequenceClip(depth_images, fps=fps)
+            clip = depth_clip
+        if args['rgb']:
+            rgb_images = list(data['image'])
+            rgb_images = ConvertImageListToNumpy(np.squeeze(rgb_images), format='list')
+            if args['matplotlib']:
+                draw_matplotlib(rgb_images, fps)
+            rgb_clip = mpye.ImageSequenceClip(rgb_images, fps=fps)
+            clip = rgb_clip
+        
+        if args['rgb'] and args['depth']:
+            clip = mpye.clips_array([[rgb_clip, depth_clip]])
+        
+        if args['preview']:
+            clip.preview()
 
-
-        gif_filename = example_filename.replace('.h5f','.' + args['convert'])
-        npy_to_video(images, gif_filename, fps=10, preview=args['preview'], convert=args['convert'])
+        save_filename = example_filename.replace('.h5f','.' + args['convert'])
+        if 'gif' in args['convert']:
+            clip.write_gif(save_filename)
+        elif args['convert']:
+            clip.write_videofile(filename)
 
 if __name__ == "__main__":
     args = _parse_args()
