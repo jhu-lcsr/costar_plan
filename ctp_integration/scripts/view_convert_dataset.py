3eb48ef290bd818754badb26b7475df5a29e407e
Andrew Hundt <athundt@gmail.com>
2018-05-11 13:32:22 -0400

    view_convert_dataset.py  handle dataset errors (e.g. empty) during display

--- ctp_integration/scripts/view_convert_dataset.py
+++ ctp_integration/scripts/view_convert_dataset.py
@@ -89,7 +89,7 @@ def main(args,root="root"):
     if '.h5f' in args['path']:
         filenames = [args['path']]
     else:
-        filenames = os.listdir(args['path'])
+        filenames = os.listdir(os.path.expanduser(args['path']))
 
     # Read data
     progress_bar = tqdm(filenames)
@@ -112,38 +112,48 @@ def main(args,root="root"):
             example_filename = os.path.join(args['path'], filename)
         else:
             example_filename = filename
-        progress_bar.set_description("Processing %s" % example_filename)
+
+        description = "Processing %s" % example_filename
+        progress_bar.set_description(description)
+        pygame.display.set_caption(description)
 
         data = h5py.File(example_filename,'r')
 
         fps=args['fps']
+        # check if the data is there to load
+        load_depth = args['depth'] and 'depth_image' in data and len(data['depth_image']) > 0
+        load_rgb = args['rgb'] and 'image' in data and len(data['depth_image']) > 0
 
-        if args['depth']:
-            depth_images = list(data['depth_image'])
-            depth_images = ConvertImageListToNumpy(np.squeeze(depth_images), format='list')
-            if args['matplotlib']:
-                draw_matplotlib(depth_images, fps)
-            depth_clip = mpye.ImageSequenceClip(depth_images, fps=fps)
-            clip = depth_clip
-        if args['rgb']:
-            rgb_images = list(data['image'])
-            rgb_images = ConvertImageListToNumpy(np.squeeze(rgb_images), format='list')
-            if args['matplotlib']:
-                draw_matplotlib(rgb_images, fps)
-            rgb_clip = mpye.ImageSequenceClip(rgb_images, fps=fps)
-            clip = rgb_clip
-        
-        if args['rgb'] and args['depth']:
-            clip = mpye.clips_array([[rgb_clip, depth_clip]])
-        
-        if args['preview']:
-            clip.preview()
+        try:
+            if load_depth:
+                depth_images = list(data['depth_image'])
+                depth_images = ConvertImageListToNumpy(np.squeeze(depth_images), format='list')
+                if args['matplotlib']:
+                    draw_matplotlib(depth_images, fps)
+                depth_clip = mpye.ImageSequenceClip(depth_images, fps=fps)
+                clip = depth_clip
+            if load_rgb:
+                rgb_images = list(data['image'])
+                rgb_images = ConvertImageListToNumpy(np.squeeze(rgb_images), format='list')
+                if args['matplotlib']:
+                    draw_matplotlib(rgb_images, fps)
+                rgb_clip = mpye.ImageSequenceClip(rgb_images, fps=fps)
+                clip = rgb_clip
+            
+            if load_depth and load_rgb:
+                clip = mpye.clips_array([[rgb_clip, depth_clip]])
+            
+            if args['preview']:
+                clip.preview()
 
-        save_filename = example_filename.replace('.h5f','.' + args['convert'])
-        if 'gif' in args['convert']:
-            clip.write_gif(save_filename)
-        elif args['convert']:
-            clip.write_videofile(filename)
+            save_filename = example_filename.replace('.h5f','.' + args['convert'])
+            if 'gif' in args['convert']:
+                clip.write_gif(save_filename)
+            elif args['convert']:
+                clip.write_videofile(filename)
+        except Exception as ex:
+            print('Exception encountered while processing ' + example_filename + 
+                  ' please edit the code to debug the specifics: ' + str(ex))
 
 if __name__ == "__main__":
     args = _parse_args()
