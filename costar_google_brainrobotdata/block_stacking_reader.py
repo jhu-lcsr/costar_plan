e90bfaab5b1d2a152f1a329ba6a4cda3bc7b13f8
Andrew Hundt <ATHundt@gmail.com>
2018-07-01 00:28:02 -0400

    block_stacking_reader.py whitespace

--- costar_google_brainrobotdata/block_stacking_reader.py
+++ costar_google_brainrobotdata/block_stacking_reader.py
@@ -378,7 +378,7 @@ class CostarBlockStackingSequence(Sequence):
                     'current_xyz_3' in self.data_features_to_extract or
                     'image_0_image_n_vec_xyz_10' in self.data_features_to_extract):
                 # default, regression input case
-                action_poses_vec = np.concatenate([encoded_poses[:,:3], action_labels], axis=-1)
+                action_poses_vec = np.concatenate([encoded_poses[:, :3], action_labels], axis=-1)
                 X = [init_images, current_images, action_poses_vec]
             elif (self.data_features_to_extract is None or
                     'current_xyz_aaxyz_nsc_8' in self.data_features_to_extract or
@@ -404,7 +404,7 @@ class CostarBlockStackingSequence(Sequence):
             if self.label_features_to_extract is None or 'grasp_goal_xyz_3' in self.label_features_to_extract:
                 # default, regression to translation case, see semantic_translation_regression in cornell_grasp_train.py
                 y = grasp_metrics.batch_encode_xyz_qxyzw_to_xyz_aaxyz_nsc(y, random_augmentation=self.random_augmentation)
-                y = y[:,:3]
+                y = y[:, :3]
             elif self.label_features_to_extract is None or 'grasp_goal_xyz_aaxyz_nsc_8' in self.label_features_to_extract:
                 # default, regression label case
                 y = grasp_metrics.batch_encode_xyz_qxyzw_to_xyz_aaxyz_nsc(y, random_augmentation=self.random_augmentation)
