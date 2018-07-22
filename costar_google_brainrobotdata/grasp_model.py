e77374542fbbcd5ae22610a75234d4a3e4f6208d
Andrew Hundt <ATHundt@gmail.com>
2018-07-22 22:50:15 +0100

    grasp_model.py add extra image input channels for coord_conv_img case

--- costar_google_brainrobotdata/grasp_model.py
+++ costar_google_brainrobotdata/grasp_model.py
@@ -592,6 +592,9 @@ def choose_hypertree_model(
             )
         else:
             image_input_shape = image_shapes[0]
+        if coordinate_data is not None and coordinate_data == 'coord_conv_img':
+            # coord_conv_img case currently adds 2 additional channels.
+            image_input_shape[2] = image_input_shape[2] + 2
         print('hypertree image_input_shape: ' + str(image_input_shape))
         print('hypertree images: ' + str(images))
         print('hypertree classes: ' + str(classes))
