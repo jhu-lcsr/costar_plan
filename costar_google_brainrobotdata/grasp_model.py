cf0fe7fa0a113add9d3d209bc775ec24db27cacc
Andrew Hundt <ATHundt@gmail.com>
2018-07-23 12:18:25 +0100

    grasp_model.py create_image_tensor try removing input which may not be needed

--- costar_google_brainrobotdata/grasp_model.py
+++ costar_google_brainrobotdata/grasp_model.py
@@ -758,7 +758,7 @@ def choose_hypertree_model(
             if (coordinate_data is not None and
                     coordinate_data is not 'none' and
                     coordinate_data is 'coord_conv_img'):
-                tensor = Input(tensor=tensor)
+                # tensor = Input(tensor=tensor)
                 tensor = coord_conv.CoordinateChannel2D()(tensor)
             if image_model_weights == 'shared':
                 ImageModelCarrier.image_models += [image_model]
