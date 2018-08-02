7f5f57831cdfad2da009e7ed418ff98e7b13737c
Andrew Hundt <ATHundt@gmail.com>
2018-08-02 16:14:56 -0400

    add setup.py for costar_hyper

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''
+Run this one if you just want a python package.
+'''
+
+from setuptools import setup, find_packages
+setup(name='costar_hyper',
+      version='0.0.1',
+      description='Deep Learning For Robotic Tasks with HyperTree Search',
+      author='Andrew Hundt',
+      author_email='ahundt@jhu.edu',
+      url='https://github.com/cpaxton/costar_plan',
+      packages=find_packages())
