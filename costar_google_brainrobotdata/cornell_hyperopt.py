0e52a29f6e270b06e9ebf6f720d5ea8f55747888
Andrew Hundt <ATHundt@gmail.com>
2018-08-20 14:18:15 -0400

    nasnet only translation hyperopt search

--- costar_google_brainrobotdata/cornell_hyperopt.py
+++ costar_google_brainrobotdata/cornell_hyperopt.py
@@ -65,7 +65,8 @@ def main(_):
     FLAGS.dataset_name = 'costar_block_stacking'
     # FLAGS.problem_type = 'semantic_grasp_regression'
     FLAGS.log_dir = 'hyperopt_logs_costar_block_stacking_train_ranked_regression'
-    FLAGS.problem_type = 'semantic_rotation_regression'
+    # FLAGS.problem_type = 'semantic_rotation_regression'
+    FLAGS.problem_type = 'semantic_translation_regression'
     FLAGS.batch_size = 16
     FLAGS.num_validation = 1
     FLAGS.num_test = 1
