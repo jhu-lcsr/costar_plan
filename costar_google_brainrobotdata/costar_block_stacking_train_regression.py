1aa2b03592fc6a2bf9e48b67984701ee13b89f87
Andrew Hundt <ATHundt@gmail.com>
2018-08-31 20:29:36 -0400

    costar_block_stacking_train_regression.py train rotation model from scratch with msle

--- costar_google_brainrobotdata/costar_block_stacking_train_regression.py
+++ costar_google_brainrobotdata/costar_block_stacking_train_regression.py
@@ -19,8 +19,8 @@ FLAGS = flags.FLAGS
 def main(_):
     use_best_model = True
     load_best_weights = False
-    problem_type = 'semantic_translation_regression'
-    # problem_type = 'semantic_rotation_regression'
+    # problem_type = 'semantic_translation_regression'
+    problem_type = 'semantic_rotation_regression'
     # problem_type = 'semantic_grasp_regression'
     # problem_type2 = 'semantic_grasp_regression'
     problem_type2 = None
