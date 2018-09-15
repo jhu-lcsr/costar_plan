3b66d3fe84367dd4d516ea36ab372ea3f7f3287d
Andrew Hundt <ATHundt@gmail.com>
2018-09-15 17:35:12 -0400

    block_stacking_reader.py bugfixes from ENAS repository

--- costar_hyper/block_stacking_reader.py
+++ costar_hyper/block_stacking_reader.py
@@ -344,10 +344,6 @@ def encode_action_and_images(
     else:
         raise ValueError('Unsupported data input: ' + str(data_features_to_extract))
 
-    if (data_features_to_extract is not None and 'image_0_image_n_vec_xyz_aaxyz_nsc_15' in data_features_to_extract):
-        # make the giant data cube if it is requested
-        X = concat_images_with_tiled_vector_np(X[:2], X[2:])
-
     if (data_features_to_extract is not None and
             ('image_0_image_n_vec_xyz_10' in data_features_to_extract or
              'image_0_image_n_vec_xyz_aaxyz_nsc_15' in data_features_to_extract or
@@ -747,7 +743,10 @@ class CostarBlockStackingSequence(Sequence):
             # print("shape=====",X.shape)
 
             # determine the label
-            y = encode_label(self.label_features_to_extract, y, action_successes, self.random_augmentation, current_stacking_reward)
+            if('stacking_reward' in self.label_features_to_extract):
+                y = encode_label(self.label_features_to_extract, y, action_successes, self.random_augmentation, current_stacking_reward)
+            else:
+                y = encode_label(self.label_features_to_extract, y, action_successes, self.random_augmentation, None)
 
             # Debugging checks
             if X is None:
