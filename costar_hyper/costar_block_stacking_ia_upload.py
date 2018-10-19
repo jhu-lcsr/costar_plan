26ba4cd72b731637f38044a76393e78f39e9aca7
Andrew Hundt <athundt@gmail.com>
2018-10-19 19:21:45 -0400

    costar_block_stacking_ia_upload.py improved recordkeeping, exec param works

--- costar_hyper/costar_block_stacking_ia_upload.py
+++ costar_hyper/costar_block_stacking_ia_upload.py
@@ -1,6 +1,15 @@
 import internetarchive
 import argparse
 import os
+import datetime
+
+
+def timeStamped(fname, fmt='%Y-%m-%d-%H-%M-%S_{fname}'):
+    """ Apply a timestamp to the front of a filename description.
+
+    see: http://stackoverflow.com/a/5215012/99379
+    """
+    return datetime.datetime.now().strftime(fmt).format(fname=fname)
 
 
 def _parse_args():
@@ -22,33 +31,33 @@ def main(args, root='root'):
     item = internetarchive.get_item('costar_block_stacking_dataset')
 
     md = dict(
-        # TODO(rexxarchl): change to Dataset Collection after proper testing
-        # collection='datasets',
-        collection='test_collection',
+        # TODO(rexxarchl): change to Dataset Collection 'datasets' after proper testing
+        collection='datasets',
+        # collection='test_collection',
         title='The CoSTAR Block Stacking Dataset',
         version='v0.4',  # Custom metadata field for the current version
         contributor='Andrew Hundt, Varun Jain, Chris Paxton, Chunting Jiao, '
                     'Chia-Hung Lin, and Gregory D. Hager',
         creator='Andrew Hundt <ATHundt@gmail.com>',
         credits='''
-                Andrew Hundt, Varun Jain, Chris Paxton, Chunting Jiao, Chia-Hung Lin, 
+                Andrew Hundt, Varun Jain, Chris Paxton, Chunting Jiao, Chia-Hung Lin,
                 and Gregory D. Hager<br>
                 The Johns Hopkins University<br>
-                <a href="https://cirl.lcsr.jhu.edu/">Computational Interaction and 
+                <a href="https://cirl.lcsr.jhu.edu/">Computational Interaction and
                 Robotics Laboratory</a><br>
-                This material is based upon work supported by the National Science 
+                This material is based upon work supported by the National Science
                 Foundation under NRI Grant Award No. 1637949.
                 ''',
         date='2018-10-17',
         description='''
-            Stack blocks like a champion! The CoSTAR Block Stacking Dataset includes a 
-            real robot trying to stack colored children's blocks more than 10,000 times 
-            in a scene with challenging lighting and a movable bin obstacle which must 
-            be avoided. This dataset is especially well suited to the benchmarking and 
+            Stack blocks like a champion! The CoSTAR Block Stacking Dataset includes a
+            real robot trying to stack colored children's blocks more than 10,000 times
+            in a scene with challenging lighting and a movable bin obstacle which must
+            be avoided. This dataset is especially well suited to the benchmarking and
             comparison of deep learning algorithms.<br>
-            Visit the <a href='https://sites.google.com/site/costardataset'>website</a> 
+            Visit the <a href='https://sites.google.com/site/costardataset'>website</a>
             for more info.<br>
-            <b>Cite: </b><a href='https://sites.google.com/view/hypertree-renas'>Training 
+            <b>Cite: </b><a href='https://sites.google.com/view/hypertree-renas'>Training
             Frankenstein's Creature to Stack: HyperTree Architecture Search</a>
             ''',
         license='https://creativecommons.org/licenses/by/4.0/',
@@ -56,24 +65,42 @@ def main(args, root='root'):
         noindex='True')  # Set to true for the item to not be listed
 
     print(args)
-    path = os.path.expanduser(args['path'])
+    # get the path, and add an extra slash to make sure it is a directory
+    # and to avoid a bug in the internet archive upload code.
+    path = os.path.expanduser(args['path'] + '/')
 
-    r = item.upload(
-            path,
-            metadata=md,
-            verify=True,  # Verify local MD5 checksum matches remote MD5 checksum
-            checksum=True,  # Skip files based on checksum
-            verbose=True,  # Print progress to stdout
-            retries=100,  # Number of times to retry the given request
-            retries_sleep=5,  # Amount of time to sleep between `retries`
-            # Prevent an item from being derived to another format after upload
-            queue_derive=False,
-            # Set to true to print headers to stdout, and exit without uploading
-            # debug = args['execute'])
-            debug=True)
+    print('uploading all data from path:\n\n ' + str(path))
 
-    print(r)
+    results = item.upload(
+        path,
+        metadata=md,
+        verify=True,  # Verify local MD5 checksum matches remote MD5 checksum
+        checksum=True,  # Skip files based on checksum
+        verbose=True,  # Print progress to stdout
+        retries=100,  # Number of times to retry the given request
+        retries_sleep=5,  # Amount of time to sleep between `retries`
+        # Prevent an item from being derived to another format after upload
+        queue_derive=False,
+        # Set to true to print headers to stdout, and exit without uploading
+        debug=args['execute'])
 
+    server_urls = [str(result.url) for result in results]
+    local_urls = [str(result.path_url) for result in results]
+    prefix = timeStamped('internet_archive_uploaded')
+    server_txt = prefix + '_server_urls.txt'
+    local_txt = prefix + '_local_path_urls.txt'
+    with open(server_txt, mode='w') as set_file:
+        set_file.write('\n'.join(server_urls))
+    with open(local_txt, mode='w') as set_file:
+        set_file.write('\n'.join(local_urls))
+
+    print('-' * 80)
+    print('local_urls:' + str(local_urls))
+    print('-' * 80)
+    print('server_urls:' + str(server_urls))
+    print('-' * 80)
+    print('internet archive upload complete! file lists are in: \n' +
+          str(server_txt) + '\n' + str(local_txt))
 
 if __name__ == '__main__':
     args = _parse_args()
